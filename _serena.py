3c55cdd2c6ae7732c176eaef0d424e29fd14a33d
David <kyran36@hotmail.com>
2014-05-17 18:44:39 +0900

    made a few text navigation macros easier to say

--- _serena.py
+++ _serena.py
@@ -4,7 +4,7 @@ import natlink,os,win32gui
 MACRO_PATH='C:\NatLink\NatLink\MacroSystem\\'
 
 def foo():
-   natlink.execScript ("TTSPlayString \"" +"I am Serena, version 0.1.58"+ "\"")
+   natlink.execScript ("TTSPlayString \"" +"I am Serena, version 0.1.59"+ "\"")
    
 def repeat_after(text):
    natlink.execScript ("TTSPlayString \"" +str(text)+ "\"")
