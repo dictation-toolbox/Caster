c8bee8666a5a9999235fb656503f1714e0490a34
David <kyran36@hotmail.com>
2014-08-07 08:40:52 +0900

    banls

--- element_.py
+++ element_.py
@@ -20,7 +20,7 @@ New Future Features List:
 
 class Element:
     def __init__(self):
-       
+        
         # setup basics
         self.JSON_PATH=paths.get_element_json_path()
         self.TOTAL_SAVED_INFO=utilities.load_json_file(self.JSON_PATH)
@@ -42,9 +42,8 @@ class Element:
         self.PYTHON_FUNCTIONS=re.compile("(\bdef \b([A-Za-z0-9_]+)\()|(\.([A-Za-z0-9_]+)\()")# cgi 1 or 3
         self.PYTHON_VARIABLES=re.compile("(([A-Za-z0-9_]+)[ ]*=)|((\(|,| )([A-Za-z0-9_]+)(\)|,| ))")# 1 or 4
         # java language
-        self.JAVA_IMPORTS=re.compile("")# don't forget things following the word "new"
-        self.JAVA_METHODS=re.compile("")
-        # to do: add detection for parameters in methods
+        self.JAVA_IMPORTS=re.compile("\bimport\b[A-Za-z0-9_\. ]+\.([A-Za-z0-9_]+);|\bthrows \b([A-Za-z0-9_]+)|([A-Za-z0-9_]+)\.|\bnew \b([A-Za-z0-9_\<\>]+)")
+        self.JAVA_METHODS=re.compile("[ \.]([A-Za-z0-9_]+)\(")
         self.JAVA_VARIABLES=re.compile("([ \.]*([A-Za-z0-9_]+)[ ]*=)|((\bpublic\b|\bprivate\b|\binternal\b|\bfinal\b|\bstatic\b)[ ]+[A-Za-z0-9_]+[ ]+([A-Za-z0-9_]+)[ ]*[;=])|(([A-Za-z0-9_]+)[ ]*[,\)])")#1,4,6
         
         # setup tk
@@ -219,16 +218,21 @@ class Element:
     
     
     def populate_list(self, file_to_activate):
-
+        # get the selected directory
         selected_directory=self.dropdown_selected.get()
-        self.current_file=None
         if selected_directory==self.default_dropdown_message:
-            return#  if a scanned for hasn't been selected, there's no need to go any further
-        self.clear_lists()
+            return#  if a scanned directory hasn't been selected, there's no need to go any further
+        
+        # check to see if the focused file in the active window is among those scanned in the selected directory
+        # -- if it is, set it to self.current_file
+        self.current_file=None
         for absolute_path in self.TOTAL_SAVED_INFO["directories"][selected_directory]["files"]:
             if absolute_path.endswith("/"+file_to_activate) or absolute_path.endswith("\\"+file_to_activate):
                 self.current_file=self.TOTAL_SAVED_INFO["directories"][selected_directory]["files"][absolute_path]
                 break
+        
+        # update accordingly
+        self.clear_lists()
         if not self.current_file==None:
             self.reload_list(self.current_file["names"],self.current_file["sticky"])
     
@@ -263,9 +267,13 @@ class Element:
             
     def select_from_dropdown(self, key, save=True):
         self.TOTAL_SAVED_INFO["config"]["last_directory"]=key
+        
         if save:
             utilities.save_json_file(self.TOTAL_SAVED_INFO, self.JSON_PATH)
         self.dropdown_selected.set(key)
+        self.ext_box.delete(0, tk.END)
+        self.ext_box.insert(0, ",".join(self.TOTAL_SAVED_INFO["directories"][key]["extensions"]))
+        self.clear_lists()
         
     def populate_dropdown(self):
         self.TOTAL_SAVED_INFO=utilities.load_json_file(self.JSON_PATH)
@@ -295,11 +303,12 @@ class Element:
                         # check for old information on the same file, save it if it exists
                         old_sticky_list=[]
                         old_added_list=[]
+                        old_banned_list=[]
                         if "directories" in self.TOTAL_SAVED_INFO:
-                            if base in self.TOTAL_SAVED_INFO["directories"]:
-                                if absolute_path in self.TOTAL_SAVED_INFO["directories"][base]["files"]:
-                                    old_sticky_list=self.TOTAL_SAVED_INFO["directories"][base]["files"][absolute_path]["sticky"]
-                                    old_added_list=self.TOTAL_SAVED_INFO["directories"][base]["files"][absolute_path]["added"]
+                            if absolute_path in self.TOTAL_SAVED_INFO["directories"][directory]["files"]:
+                                old_sticky_list=self.TOTAL_SAVED_INFO["directories"][directory]["files"][absolute_path]["sticky"]
+                                old_added_list=self.TOTAL_SAVED_INFO["directories"][directory]["files"][absolute_path]["added"]
+                                old_banned_list=self.TOTAL_SAVED_INFO["directories"][directory]["files"][absolute_path]["banned"]
 
                         
                         # set up dictionary for new scan      
@@ -307,7 +316,8 @@ class Element:
                         scanned_file["filename"]=fname
                         scanned_file["names"]=[]
                         scanned_file["sticky"]=["","","","","","","","","",""]
-                        scanned_file["added"]=[]
+                        scanned_file["added"]=old_added_list
+                        scanned_file["banned"]=old_banned_list
                         
                         # add old sticky list back in
                         if len(filter(lambda i: not i=="",old_sticky_list))>0:# if there are any nonblank values in old_sticky_list
@@ -317,14 +327,15 @@ class Element:
                         f = open(base+"/"+fname, "r")
                         lines = f.readlines()
                         f.close()
+                        if fname=="Deck.java":
+                            print " breakpoint"
                         for line in lines:
                             filter_results=self.filter(line, extension)
                             for result in filter_results:                                
-                                if not (result in scanned_file["names"] or result in scanned_file["sticky"] or result in old_sticky_list) and not result=="":
+                                if not (result in scanned_file["banned"] or result in scanned_file["names"] or result in scanned_file["sticky"] or result in old_sticky_list) and not result=="":
                                     scanned_file["names"].append(result)
                         
-                        # re-add stuff that was manually added before
-                        scanned_file["added"]=old_added_list
+                        # re-add stuff that was manually added before, The added list can override the ban list
                         for added_name in scanned_file["added"]:
                             if not (added_name in scanned_file["names"] or added_name in scanned_file["sticky"] or added_name in old_sticky_list) and not added_name=="":
                                 scanned_file["names"].append(added_name)
@@ -356,7 +367,7 @@ class Element:
         function_indices=None
         variable_indices=None
         
-        # Python        
+        # determine language and set up regular expressions        
         if extension==".py":
             import_match_object=self.PYTHON_IMPORTS.findall( line )
             function_match_object=self.PYTHON_FUNCTIONS.findall( line )
@@ -368,8 +379,8 @@ class Element:
             import_match_object=self.JAVA_IMPORTS.findall( line )
             function_match_object=self.JAVA_METHODS.findall( line )
             variable_match_object=self.JAVA_VARIABLES.findall( line )
-            import_indices=[]
-            function_indices=[]
+            import_indices=[0,1,2,3]
+            function_indices=[0]
             variable_indices=[1,4,6]
         
         results=[]
@@ -382,11 +393,14 @@ class Element:
         return results
     
     def process_match(self, match_object, desired_indices, results):
-        for match_tuple in match_object:
-            for index in desired_indices:
-                match=match_tuple[index]
-                if not (match=="" or match.isdigit() or match in results):
-                    results.append(match)
+        for m in match_object:
+            if isinstance(m, tuple):
+                for index in desired_indices:
+                    match=m[index]
+                    if not (match=="" or match.isdigit() or match in results):
+                        results.append(match)
+            elif isinstance(m, str):
+                results.append(m)
         return results
         
          
@@ -396,7 +410,7 @@ class Element:
     def process_request(self):
         request_object = json.loads(request.body.read())
         action_type=request_object["action_type"]
-        if self.current_file==None and (not action_type in ["extensions","trigger_directory_box","scan_new"]):#only these are allowed when no file is loaded
+        if self.current_file==None and (not action_type in ["extensions","trigger_directory_box","rescan","scan_new"]):#only these are allowed when no file is loaded
             return "No file is currently loaded."
         if "index" in request_object:
             index=int(request_object["index"])
@@ -439,6 +453,7 @@ class Element:
                     self.current_file["names"].remove(target_word)
                 if target_word in self.current_file["added"]:
                     self.current_file["added"].remove(target_word)
+                self.current_file["banned"].append(target_word)
                 utilities.save_json_file(self.TOTAL_SAVED_INFO, self.JSON_PATH)
                 self.root.after(10, lambda: self.populate_list(self.last_file_loaded))
                 return "c"
