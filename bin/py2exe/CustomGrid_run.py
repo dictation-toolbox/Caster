eaf733cb25ffcf9a66c6986ec3930907e1f29e61
David <kyran36@hotmail.com>
2014-11-28 02:01:54 +0900

    deleted py2exe stuff

--- bin/py2exe/CustomGrid_run.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup
-import py2exe,os
-
-setup(	options = {'py2exe': {'bundle_files': 1}},
-		windows=[{"script":"C:\\NatLink\\NatLink\\MacroSystem\\bin\\homebrew\\CustomGrid\\CustomGrid.py", 'icon_resources':[(1,'icon.ico')]}]
-	)
