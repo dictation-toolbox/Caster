3d89a281d2919bfa6b82b68281d2326686d54a16
David Conway <dconway1985@gmail.com>
2015-09-08 21:52:32 -0400

    added core folder

---
+++ caster/lib/ccr2/core/alphabet.py
@@ -0,0 +1,20 @@
+from dragonfly import Function, Choice
+
+from caster.lib import navigation
+from caster.lib.dfplus.mergerule import MergeRule
+from caster.lib.dfplus.state.short import R
+
+
+class Alphabet(MergeRule):
+    mapping = {
+        "[<big>] <letter>": R(Function(navigation.letters2, extra ={"big", "letter"}), rdescript="Spell"),
+        }
+    extras = [
+        navigation.get_alphabet_choice("letter"),
+        Choice("big",
+              {"big": "big",
+               }),
+    ]
+    defaults = {
+    "big": "", 
+    }
---
+++ caster/lib/ccr2/core/numbers.py
@@ -0,0 +1,22 @@
+from dragonfly import Function
+
+from caster.lib import navigation
+from caster.lib.dfplus.additions import IntegerRefST
+from caster.lib.dfplus.mergerule import MergeRule
+from caster.lib.dfplus.state.short import R
+
+
+class Numbers(MergeRule):
+    mapping = {
+            "word number <wn>":     R(Function(navigation.word_number, extra="wn"), rdescript="Number As Word"),
+            "numb <wnKK>":          R(Function(navigation.numbers2, extra="wnKK"), rspec="number", rdescript="Number"),
+                     
+            
+          }
+
+
+    extras = [
+        IntegerRefST("wn", 0, 10),
+        IntegerRefST("wnKK", 0, 1000000),
+    ]
+    defaults = {}
