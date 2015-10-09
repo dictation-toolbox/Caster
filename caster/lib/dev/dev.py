7b1f65d24f10f6ebba3ca2beb7489f154908e6dc
David Conway <dconway1985@gmail.com>
2015-10-08 21:30:42 -0400

    restructured dev.py

--- caster/lib/dev/dev.py
+++ caster/lib/dev/dev.py
@@ -81,80 +81,88 @@ def bring_test():
         utilities.simple_log()
         
 
-class DevRule(MappingRule):
-    
+class StackTest(MappingRule):
+    '''test battery for the ContextStack'''
     mapping = {
-    # development tools
-    "(show | open) documentation":  BringApp(settings.SETTINGS["paths"]["DEFAULT_BROWSER_PATH"]) + WaitWindow(executable=settings.get_default_browser_executable()) + Key('c-t') + WaitWindow(title="New Tab") + Text('http://dragonfly.readthedocs.org/en/latest') + Key('enter'),
-
-    "open natlink folder":          R(BringApp("C:/Windows/explorer.exe", settings.SETTINGS["paths"]["BASE_PATH"].replace("/", "\\")), rdescript="Open Natlink Folder"),
-    "reserved word <text>":         Key("dquote,dquote,left") + Text("%(text)s") + Key("right, colon, tab/5:5") + Text("Text(\"%(text)s\"),"),
-    "refresh debug file":           Function(devgen.refresh),  
-    "Agrippa <filetype> <path>":    Function(grep_this),
-    
-    # experimental/incomplete commands
-    
-    "experiment <text>":            Function(experiment),
-    "short talk number <n2>":       Text("%(n2)d"), 
-#     "dredge [<id> <text>]":         Function(dredge),
-    
-    "close last tag":               ContextSeeker([L(S(["cancel"], None),
-                                                     S(["html spoken"], close_last_spoken, use_spoken=True), 
-                                                     S(["span", "div"], close_last_rspec, use_rspec=True))
-                                                   ]),
-    "html":                         R(Text("<html>"), rspec="html spoken"), 
-    "divider":                      R(Text("<div>"), rspec="div"),
-    "span":                         R(Text("<span>"), rspec="span"),
-    "backward seeker [<text>]":     ContextSeeker([L(S(["ashes"], Text("ashes1 [%(text)s] ")),
-                                                      S(["bravery"], Text("bravery1 [%(text)s] "))), 
-                                                   L(S(["ashes"], Text("ashes2 [%(text)s] ")),
-                                                      S(["bravery"], Text("bravery2 [%(text)s] ")))
-                                                   ]), 
-    "forward seeker [<text>]":      ContextSeeker(forward=
-                                                  [L(S(["ashes"], Text("ashes1 [%(text)s] ")),
-                                                      S(["bravery"], Text("bravery1 [%(text)s] "))), 
-                                                   L(S(["ashes"], Text("ashes2 [%(text)s] ")),
-                                                      S(["bravery"], Text("bravery2 [%(text)s] ")))
-                                                   ]),
-    "never-ending":                 AsynchronousAction([L(S(["ashes", "charcoal"], print_time, None),
-                                                      S(["bravery"], Text, "bravery1"))
-                                                   ], time_in_seconds=0.2, repetitions=20, 
-                                                       finisher=Text("finisher successful")),
-    "ashes":                        RegisteredAction(Text("ashes fall "), rspec="ashes"),
-    "bravery":                      RegisteredAction(Text("bravery is weak "), rspec="bravery"),
-    "charcoal boy <text> [<n>]":    R(Text("charcoal is dirty %(text)s"), rspec="charcoal"),
-                            
-    "test confirm action":          ConfirmAction(Key("a"), rdescript="Confirm Action Test"),
-    
-    "test box action":              BoxAction(lambda data: None, rdescript="Test Box Action", box_type=settings.QTYPE_DEFAULT, 
-                                              log_failure=True),
-    
-    "test dragonfly paste":         Paste("some text"),
-     
+        "close last tag":               ContextSeeker([L(S(["cancel"], None),
+                                                         S(["html spoken"], close_last_spoken, use_spoken=True), 
+                                                         S(["span", "div"], close_last_rspec, use_rspec=True))
+                                                       ]),
+        "html":                         R(Text("<html>"), rspec="html spoken"), 
+        "divider":                      R(Text("<div>"), rspec="div"),
+        "span":                         R(Text("<span>"), rspec="span"),
+        "backward seeker [<text>]":     ContextSeeker([L(S(["ashes"], Text("ashes1 [%(text)s] ")),
+                                                          S(["bravery"], Text("bravery1 [%(text)s] "))), 
+                                                       L(S(["ashes"], Text("ashes2 [%(text)s] ")),
+                                                          S(["bravery"], Text("bravery2 [%(text)s] ")))
+                                                       ]), 
+        "forward seeker [<text>]":      ContextSeeker(forward=
+                                                      [L(S(["ashes"], Text("ashes1 [%(text)s] ")),
+                                                          S(["bravery"], Text("bravery1 [%(text)s] "))), 
+                                                       L(S(["ashes"], Text("ashes2 [%(text)s] ")),
+                                                          S(["bravery"], Text("bravery2 [%(text)s] ")))
+                                                       ]),
+        "never-ending":                 AsynchronousAction([L(S(["ashes", "charcoal"], print_time, None),
+                                                          S(["bravery"], Text, "bravery1"))
+                                                       ], time_in_seconds=0.2, repetitions=20, 
+                                                           finisher=Text("finisher successful")),
+        "ashes":                        RegisteredAction(Text("ashes fall "), rspec="ashes"),
+        "bravery":                      RegisteredAction(Text("bravery is weak "), rspec="bravery"),
+        "charcoal boy <text> [<n>]":    R(Text("charcoal is dirty %(text)s"), rspec="charcoal"),
+                                
+        "test confirm action":          ConfirmAction(Key("a"), rdescript="Confirm Action Test"),
+        
+        "test box action":              BoxAction(lambda data: None, rdescript="Test Box Action", box_type=settings.QTYPE_DEFAULT, 
+                                                  log_failure=True),
     }
     extras = [
               Dictation("text"),
-              Dictation("textnv"),
-              IntegerRefST("n", 1, 5),
-              Choice("id",
-                    {"R": 1, "M":2,
-                     }),
+              IntegerRefST("n", 1, 5)
+             ]
+    defaults = {"text": ""}
+
+class DevelopmentHelp(MappingRule):
+    mapping = {
+        # caster development tools
+        "(show | open) documentation":  BringApp(settings.SETTINGS["paths"]["DEFAULT_BROWSER_PATH"]) + WaitWindow(executable=settings.get_default_browser_executable()) + Key('c-t') + WaitWindow(title="New Tab") + Text('http://dragonfly.readthedocs.org/en/latest') + Key('enter'),
+        "open natlink folder":          R(BringApp("C:/Windows/explorer.exe", settings.SETTINGS["paths"]["BASE_PATH"].replace("/", "\\")), rdescript="Open Natlink Folder"),
+        "refresh debug file":           Function(devgen.refresh),  
+        "Agrippa <filetype> <path>":    Function(grep_this)
+    }
+    extras = [
+              Dictation("text"),
               Choice("path",
                     {"natlink": "c:/natlink/natlink", "sea":"C:/",
                      }),
               Choice("filetype",
                     {"java": "*.java", "python":"*.py",
-                     }),
+                     })
+             ]
+    defaults = {"text": ""}
+
+class Experimental(MappingRule):
+    
+    mapping = {
+        # experimental/incomplete commands
+        
+        "experiment <text>":            Function(experiment),
+        "short talk number <n2>":       Text("%(n2)d"), 
+    #     "dredge [<id> <text>]":         Function(dredge),
+        "test dragonfly paste":         Paste("some text"),
+     
+    }
+    extras = [
+              Dictation("text"),
               IntegerRefST("n2", 1, 100)
              ]
-    defaults = {
-               "text": "", "id":None
-               }
+    defaults = {"text": ""}
 
 # .ending
 def load():
     global grammar
-    grammar.add_rule(DevRule())
+    grammar.add_rule(StackTest())
+    grammar.add_rule(DevelopmentHelp())
+    grammar.add_rule(Experimental())
     grammar.load()
 
 if settings.SETTINGS["miscellaneous"]["dev_commands"]:
