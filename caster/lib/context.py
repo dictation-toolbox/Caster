1e5ceeac866ac38f1f343a18b53c031daae28c5b
David Conway <dconway1985@gmail.com>
2015-10-25 00:03:54 -0400

    fixes #74, clipboard errors

--- caster/lib/context.py
+++ caster/lib/context.py
@@ -5,59 +5,18 @@ from dragonfly import *
 from caster.lib import utilities, settings
 from caster.lib import control
 
-def navigate_to_character(direction3, target):
-    # to do: possibly speed up the keypresses by figuring out how many lines up or down to go first
-    try:
-        left_or_right = str(direction3)
-        look_left = left_or_right == "left"
-        is_character = False
-        for s in target.split("~"):
-            if s in ".,()[]{}<>":
-                is_character=True
-                break
-        
-        # make sure nothing is highlighted to boot
-        Key("right, left" if look_left else "left, right").execute()
-        if look_left:
-            Key("cs-left").execute()
-        else:
-            Key("cs-right").execute()
-        index = -1
-        context = None
-        tries=0
-        while context is None:
-            tries+=1
-            results = read_selected_without_altering_clipboard()
-            error_code = results[0]
-            if error_code==0:
-                context = results[1]
-                break
-            if tries>5:
-                return False
-        
-        # if we got to this point, we have a copy result, 
-        index = find_index_in_context(target, context, look_left)
-        
-        # highlight only the target
-        if index != -1:
-            Key("left" if look_left else "right").execute()
-            nt = index if look_left else len(context) - index - 1  # number of times to press left or right before the highlight
-            if nt != 0:
-                Key("right/5:" + str(nt) if look_left else "left/5:" + str(nt)).execute()
-            if is_character:
-                Key("s-right" if look_left else "s-left").execute()
-            else:
-                Key("cs-right" if look_left else "cs-left").execute()
+def _target_is_character(target):
+    '''determines if the target is a single character'''
+    if len(target) == 1:
+        return True
+    '''if the character is in the character group:'''
+    for s in target.split("~"):
+        if s in ".,()[]{}<>":
             return True
-        else:
-            # reset cursor
-            Key("left" if look_left else "right").execute()
-            return False
-            
-    except Exception:
-        utilities.simple_log(False)
+    return False
 
-def find_index_in_context(target, context, look_left):
+def _find_index_in_context(target, context, look_left):
+    '''attempts to find index of target in clipboard content'''
     tlist = target.split("~")
     index = -1
     if look_left:
@@ -75,6 +34,60 @@ def find_index_in_context(target, context, look_left):
     if index == 99999 or index == -99999:
         return -1
     return index
+
+def navigate_to_character(direction3, target, fill=False):
+    try:
+        look_left = str(direction3) == "left"
+        
+        # make sure nothing is highlighted to boot
+        if not fill:# (except when doing "fill" -- if at end of line, there is no space for this )
+            Key("right, left" if look_left else "left, right").execute()
+        if look_left:
+            Key("cs-left").execute()
+        else:
+            Key("cs-right").execute()
+        
+        context = None
+        tries = 0
+        while context is None:
+            tries+=1
+            results = read_selected_without_altering_clipboard()
+            error_code = results[0]
+            if error_code==0:
+                context = results[1]
+                break
+            if tries > 5:
+                return False
+        
+        # if we got to this point, we have a copy result
+        index = _find_index_in_context(target, context, look_left)
+        
+        
+        if index != -1:# target found
+            '''move the cursor to the left of the target if looking left, 
+            to the right of the target if looking right:'''
+            Key("left" if look_left else "right").execute()
+            '''number of times to press left or right before the highlight
+            (the target may be a part of a fully highlighted word): '''
+            nt = index if look_left else len(context) - index - 1
+            if nt != 0:
+                Key("right/5:" + str(nt) if look_left else "left/5:" + str(nt)).execute()
+            
+            '''highlight only the target'''
+            if _target_is_character(target):
+                Key("s-right" if look_left else "s-left").execute()
+            else:
+                Key("cs-right" if look_left else "cs-left").execute()
+            return True
+        else:
+            # reset cursor
+            Key("left" if look_left else "right").execute()
+            return False
+            
+    except Exception:
+        utilities.simple_log()
+
+
 
 def read_selected_without_altering_clipboard(same_is_okay=False):
     '''Returns a tuple:
@@ -100,16 +113,15 @@ def read_selected_without_altering_clipboard(same_is_okay=False):
     except Exception:
         utilities.simple_log(False)
         return 2, None
-    
     if prior_content == temporary and not same_is_okay:
         return 1, None
     return 0, temporary
 
 
 def fill_within_line(target):
-    result = navigate_to_character("left", str(target))
+    result = navigate_to_character("left", str(target), True)
     if result:
-        control.nexus().state.halt_asynchronous(True)  # @UndefinedVariable
+        control.nexus().state.halt_asynchronous(True)  
     return result
         
 def nav(parameters):
