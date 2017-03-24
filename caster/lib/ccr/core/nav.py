86568f1cf6f87671358c8e9a74f3fc4129f88e6c
vivshaw <hvivianshaw@gmail.com>
2017-03-24 12:00:02 -0400

    Remove extra syllable from formatting commands

--- caster/lib/ccr/core/nav.py
+++ caster/lib/ccr/core/nav.py
@@ -143,10 +143,10 @@ class Navigation(MergeRule):
     "Kraken":                       R(Key("c-space"), rspec="Kraken", rdescript="Control Space"),
          
     # text formatting
-    "set format (<capitalization> <spacing> | <capitalization> | <spacing>) (bow|bowel)":  R(Function(textformat.set_text_format), rdescript="Set Text Format"),
+    "set format (<capitalization> <spacing> | <capitalization> | <spacing>)":  R(Function(textformat.set_text_format), rdescript="Set Text Format"),
     "clear caster formatting":      R(Function(textformat.clear_text_format), rdescript="Clear Caster Formatting"),
     "peek format":                  R(Function(textformat.peek_text_format), rdescript="Peek Format"),
-    "(<capitalization> <spacing> | <capitalization> | <spacing>) (bow|bowel) <textnv> [brunt]":  R(Function(textformat.master_format_text), rdescript="Text Format"), 
+    "(<capitalization> <spacing> | <capitalization> | <spacing>) <textnv> [brunt]":  R(Function(textformat.master_format_text), rdescript="Text Format"),
     "format <textnv>":              R(Function(textformat.prior_text_format), rdescript="Last Text Format"),
     
     "dredge":                       R(Key("a-tab"), rdescript="Alt-Tab"),
