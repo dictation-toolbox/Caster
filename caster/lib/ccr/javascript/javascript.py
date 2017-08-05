d975ce8026e6bf439bfc6517b586c03c7240a1d5
vivshaw <hvivianshaw@gmail.com>
2017-08-05 01:27:35 -0400

    continue javascript overhaul, implementing a bunch of ES6 & React and some Flow

--- caster/lib/ccr/javascript/javascript.py
+++ caster/lib/ccr/javascript/javascript.py
@@ -3,7 +3,7 @@ Created on Sep 2, 2015
 
 @author: synkarius
 '''
-from dragonfly import Key, Text, Dictation, Function
+from dragonfly import Key, Text, Dictation, Function, Choice
 
 from caster.lib import control, textformat
 from caster.lib.ccr.standard import SymbolSpecs
@@ -11,6 +11,9 @@ from caster.lib.dfplus.additions import SelectiveAction
 from caster.lib.dfplus.merge.mergerule import MergeRule, TokenSet
 from caster.lib.dfplus.state.short import R
 
+# Helper methods
+def import_from(imported_from):
+    Text(textformat.master_format_text(5, 2, str(imported_from)))
 
 class Javascript(MergeRule):
     auto = [".js"]
@@ -39,10 +42,24 @@ class Javascript(MergeRule):
         SymbolSpecs.AND:                R(Text(" && "), rdescript="Javascript: And"),
         SymbolSpecs.OR:                 R(Text(" || "), rdescript="Javascript: Or"),
         SymbolSpecs.NOT:                R(Text("!"), rdescript="Javascript: Not"),
+        
         #
-        SymbolSpecs.SYSOUT:             R(Text("console.log()") + Key("left"), rdescript="Javascript: Print"),
-        #
-        # FIXME: add imports
+        SymbolSpecs.SYSOUT:             R(Text("console.log") + Key("lparen"), rdescript="JS: log"),
+        
+        # Imports
+        ## if we use just SymbolSpecs.IMPORT, then the next rule fails to recognize
+        "em" + SymbolSpecs.IMPORT:      R(Text("import  from ''") + Key("left:8"), rdescript="JS: import"),
+        "embraced" + SymbolSpecs.IMPORT:
+                                        R(Text("import {  } from ''") + Key("left:10"), rdescript="JS: import"),
+        SymbolSpecs.IMPORT + " [<is_method>] <textnv> [from <imported_from>]":
+                                        R(Text("import ") + Function(textformat.js_format)
+                                            + Text(" from ") + Key("apostrophe") + Function(import_from),
+                                            rdescript="ES6 import <x> from"),
+        "braced" + SymbolSpecs.IMPORT + " [<is_method>] <textnv> [from <imported_from>]":
+                                        R(Text("import { ") + Function(textformat.js_format)
+                                            + Text(" } from ") + Key("apostrophe") + Function(import_from),
+                                            rdescript="ES6 import <x> from"),
+
         # 
         SymbolSpecs.FUNCTION:           R(Text("function () {};") + Key("left:6, enter"), rdescript="Javascript: Function"),
         # FIXME: add classes
@@ -58,18 +75,9 @@ class Javascript(MergeRule):
         SymbolSpecs.FALSE:              R(Text("false"), rdescript="Javascript: False"),
         #
         SymbolSpecs.ARROW:              R(Text(" => "), rdescript="JS: Arrow"),
-        
-        
+
         # JavaScript specific
         "anon funk":                    R(Text("function () ") + Key("lbrace, enter"), rdescript="Javascript: Anonymous Function"),
-        "timer":                        R(Text("setInterval") + Key("lbrace"), rdescript="Javascript: Timer"),
-        "timeout":                      R(Text("setTimeout") + Key("lbrace"), rdescript="Javascript: Timeout"),
-        "document":                     R(Text("document"), rdescript="Javascript: Document"),
-        "index of":                     R(Text("indexOf") + Key("lbrace"), rdescript="Javascript: Index Of"),
-        "has own property":             R(Text("hasOwnProperty") + Key("lbrace"), rdescript="Javascript: Has Own Property"),
-        "length":                       R(Text("length"), rdescript="Javascript: Length"),
-        "self":                         R(Text("self"), rdescript="Javascript: Self"),
-        "push":                         R(Text("push"), rdescript="Javascript: Push"),
         "inner HTML":                   R(Text("innerHTML"), rdescript="Javascript: InnerHTML"),
         "new new":                      R(Text("new "), rdescript="Javascript: New"),
         "continue":                     R(Text("continue"), rdescript="Javascript: Continue"),
@@ -81,35 +89,87 @@ class Javascript(MergeRule):
         "throw":                        R(Text("throw "), rdescript="Javascript: Throw"),
         "instance of":                  R(Text("instanceof "), rdescript="Javascript: Instance Of"),
         
+
         "(far | variable)":             R(Text("var "), rdescript="Javascript: Variable"),
-        "block":                        R(Key("lbrace, enter"), rdescript="brace block"),
+        
 
         "(met | med) <textnv>":         R(Text(".")
                                             + Function(textformat.camel_format)
                                             + Key("lparen"), rdescript="JS: method call"),
-        "part <textnv>":                 R(Text(".")
-                                            + Function(textformat.camel_format),
-                                            rdescript="js: object property"),
+        "part <textnv>":                R(Text(".")
+                                            + Function(textformat.camel_format), rdescript="js: object property"),
 
         #ES6 stuff
         "(cons | const)":               R(Text("const "), rdescript="es6 const"),
         "lambda":                       R(Text("() => ") + Key("left:5"), rdescript="es6 arrow function"),
         "lambda <text>":                R(Key("lparen") + Text("%(text)s") + Key("rparen") + Text(" => "),
                                             rdescript="es6 arrow func w/ arg"),
-
+        
         # React stuff
+        "react":                        R(Text("react"), rdescript="react"),
+        "react router dom":             R(Text("react-router-dom"), rdescript="react-router"),
         "my props":                     R(Text("this.props"), rdescript="React props"),
         "my state":                     R(Text("this.state"), rdescript="React state"),
         "set state":                    R(Text("this.setState") + Key("lparen, lbrace"), rdescript="React setState"),
         "set state funk":               R(Text("this.setState") + Key("lparen"), rdescript="React functional setState"),
+        "<lifecycle>":                  R(Text("%(lifecycle)s") + Key("lparen, rparen, space, lbrace, enter"),
+                                            rdescript="React component lifecycle methods"),
+        "react constructor":            R(Text("constructor(props) ") + Key("lbrace, enter") + Text("super(props)")
+                                            + Key("enter"), rdescript="React constructor"),
+        "react (main | maine) import":  R(Text("import React, { Component } from 'react';") + Key("enter"),
+                                            rdescript="main React imports"),
+        "react dom import":             R(Text("import ReactDOM from 'react-dom';") + Key("enter"),
+                                            rdescript="React imports"),
+        "class name":                   R(Text("className"), rdescript="React: className"),
+        "browser router":               R(Text("BrowserRouter"), rdescript="react-router: BrowserRouter"),
+        "react component <textnv>":     R(Text("class ") + Function(textformat.js_format)
+                                           + Text(" extends Component ") + Key("lbrace, enter"),
+                                            rdescript="React component"),
+        "proper":                       R(Key("equals, lbrace"), rdescript="react:prop"),
+        "proper <textnv>":              R(Function(textformat.camel_format) + Key("equal, lbrace"),
+                                            rdescript="react: prop w/ name"),
+        "comp tag <textnv>":            R(Key("langle") + Function(textformat.upper_camel) + Text(" /") + Key("left:2"),
+                                            rdescript="react: self-closing component tag"),
+        "comp tag <textnv> matched":    R(Key("langle") + Function(textformat.upper_camel) + Key("rangle, langle")
+                                            + Text("/") + Function(textformat.upper_camel) + Key("c-left:3"),
+                                            rdescript="react: matched component tag"),
+
+        # flow
+        "using flow":                   R(Text("// @flow"), rdescript="enable flow"),
+        "maybe":                        R(Text(": ?"), rdescript="maybe type"),
+        "optional":                     R(Text("?: "), rdescript="optional property"),
+        
+
+
+
+        #atom/nuclide
+        "tag wrap":                     R(Key("as-w"), rdescript="tag wrap"),
           }
 
     extras = [
         Dictation("text"),
         Dictation("textnv"),
+        Dictation("imported_from"),
+        Choice("lifecycle", {
+            "react render": "render",
+            "component will mount": "componentWillMount",
+            "component did mount": "componentDidMount",
+            "component will receive props": "componentWillReceiveProps",
+            "should component update": "shouldComponentUpdate",
+            "component will update": "componentWillUpdate",
+            "component did update": "componentDidUpdate",
+            "component will unmount": "componentWillUnmount"
+            }),
+        Choice("is_method", {
+            "met": 3,
+            "ob": 2
+            })
         ]
 
-    defaults = {}
+    defaults = {
+        "is_method": 2,
+        "imported_from": ""
+    }
     
     token_set = TokenSet(["abstract", "arguments", "boolean", "break", "byte",
                  "case", "catch", "char", "class", "const", "continue",
@@ -128,9 +188,6 @@ class Javascript(MergeRule):
     
     
     
-    
-    
-    
-    
+
     
 control.nexus().merger.add_global_rule(Javascript(ID=200))
