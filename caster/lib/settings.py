34e477da4e9dac8ca7f2791b161290caac9c4c3c
Eric Paulson <Paulson.Eric@Gmail.com>
2018-01-30 23:54:08 -0800

    Fixed a typo in the fix.

--- caster/lib/settings.py
+++ caster/lib/settings.py
@@ -111,7 +111,7 @@ _DEFAULT_SETTINGS = {
         "sqldeveloper": True,
         "sublime": True,
         "visualstudio": True,
-        "visualstudiocode"": True,
+        "visualstudiocode": True,
         "winword": True,
         "wsr": True,
     },
