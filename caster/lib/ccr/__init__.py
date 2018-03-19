8320e7e08506c5b891516fa2a52538c33c1144b7
Eric Paulson <Paulson.Eric@Gmail.com>
2018-03-19 16:17:35 -0700

    Corrected imports for the new CCR modules.
    With as many times as I've broken this, I'm starting to think a test suite is a pretty good idea. I should make a list and stick it near the top.

--- caster/lib/ccr/__init__.py
+++ caster/lib/ccr/__init__.py
@@ -12,5 +12,5 @@ from javascript.javascript import Javascript
 from python.python import Python
 from rust.rust import Rust
 from sql.sql import SQL
-from vhdl import VHDL
-from prolog import Prolog
+from vhdl.vhdl import VHDL
+from prolog.prolog import Prolog
