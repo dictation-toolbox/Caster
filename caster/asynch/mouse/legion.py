ebb63aed80c6ba0012f6c163ec8b749385f03aa5
peoc peocik <sotirisdragonas@gmail.com>
2018-05-22 22:11:35 +0300

    minor fix

--- caster/asynch/mouse/legion.py
+++ caster/asynch/mouse/legion.py
@@ -18,13 +18,13 @@ if osname == "Windows":
         top_windows = []
         win32gui.EnumWindows(windowEnumerationHandler, top_windows)
         for i in top_windows:
-            if "legion" in i[1].lower():
-                print(i)
+            if "legion" in i[1].lower():                
                 win32gui.ShowWindow(i[0], 5)
                 win32gui.SetForegroundWindow(i[0])
                 break
 else:
-
+# Do nothing if in another operating system,
+# Needs Updating for different systems
     def set_legion_as_top():
         pass
 
