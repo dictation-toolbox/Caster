f58005e0a5d02ccc2cce864e40af66e018ca3a47
Digital-ecologist <42946430+Digital-ecologist@users.noreply.github.com>
2018-10-18 13:36:49 -0400

    Updated pip.bat so that it now installs dragonfly2
    I updated pip.bat so that it now installs Danesprite's dragonfly2 Python package instead of the original dragonfly.

--- caster/bin/inno/dependencies/pip.bat
+++ caster/bin/inno/dependencies/pip.bat
@@ -1,10 +1,10 @@
 @echo off
-echo Installing: pywin32, dragonfly, pillow, psutil
+echo Installing: pywin32, dragonfly2, pillow, psutil
 
 cd c:\python27\scripts
 pip install pywin32
 pip install wxpython
-pip install dragonfly
+pip install dragonfly2
 pip install pillow
 pip install psutil
 
