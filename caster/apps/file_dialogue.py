1d8a6175bf5164350d0b64c12155a475bb2dc71a
Mike Roberts <42875462+mrob95@users.noreply.github.com>
2018-12-15 20:40:11 +0000

    improvements

--- caster/apps/file_dialogue.py
+++ caster/apps/file_dialogue.py
@@ -8,28 +8,33 @@ from caster.lib.dfplus.merge import gfilter
 from caster.lib.dfplus.merge.mergerule import MergeRule
 from caster.lib.dfplus.state.short import R
 
+
 class FileDialogueRule(MergeRule):
     pronunciation = "file dialogue"
 
     mapping = {
-	    "get up":                                
-		    R(Key("a-up"), rdescript="RStudio: Navigate up"),
-		"get back":                           
-		    R(Key("a-left"), rdescript="RStudio: Navigate back"),
-		"get forward":                        
-		    R(Key("a-right"), rdescript="RStudio: Navigate forward"),
-		"file list":                         
-		    R(Key("a-d, f6, f6, f6"), rdescript="RStudio: Files list"),
-		"navigation pane":                   
-		    R(Key("a-d, f6, f6"), rdescript="RStudio: Navigation pane"),
-		"filename":                   
-		    R(Key("a-d, f6, f6, f6, f6, f6"), rdescript="RStudio: File name"),
-	}
-    extras = [
-    ]
-    defaults = {}
+        "[get] up [<n>]":
+            R(Key("a-up"), rdescript="File Dialogue: Navigate up")*Repeat(extra="n"),
+        "[get] back [<n>]":
+            R(Key("a-left"), rdescript="File Dialogue: Navigate back")*Repeat(extra="n"),
+        "[get] forward [<n>]":
+            R(Key("a-right"), rdescript="File Dialogue: Navigate forward")*Repeat(extra="n"),
+        "(files | file list)":
+            R(Key("a-d, f6:3"), rdescript="File Dialogue: Files list"),
+        "navigation [pane]":
+            R(Key("a-d, f6:2"), rdescript="File Dialogue: Navigation pane"),
+        "[file] name":
+            R(Key("a-d, f6:5"), rdescript="File Dialogue: File name"),
+    }
+    extras = [IntegerRefST("n", 1, 10)]
+    defaults = {
+        "n": 1,
+    }
 
-context = AppContext(title="save file") | AppContext(title="open file") | AppContext(title="save as")
+
+context = AppContext(title="save file") | AppContext(title="save as") | AppContext(
+    title="select folder") | AppContext(title="open file") | AppContext(title="select file")
+
 grammar = Grammar("FileDialogue", context=context)
 if settings.SETTINGS["apps"]["filedialogue"]:
     if settings.SETTINGS["miscellaneous"]["rdp_mode"]:
