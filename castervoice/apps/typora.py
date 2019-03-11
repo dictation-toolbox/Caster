3991350db35a5eab3b1429396322d312b0f0eb58
LexiconCode <TheQuietPlace@protonmail.com>
2019-03-11 14:16:44 -0500

    Finishing touches Typora
    - Commented out to duplicate commands
    - finished adding <n> and <h> to relevant commands
    - formatting adjustments

--- castervoice/apps/typora.py
+++ castervoice/apps/typora.py
@@ -15,19 +15,21 @@ from castervoice.lib.dfplus.additions import IntegerRefST
 from castervoice.lib.dfplus.state.short import R
 # ---------------------------------------------------------------------------
 
+
 class TyporaRule(MergeRule):
     pronunciation = "tie poor a"
 
     mapping = {
         # File
-        "new markdown":
+        "new file":
             R(Key("c-n"), rdescript="Typora: New Markdown Document"),
-        "new window":   
-            R(Key("cs-n"), rdescript="Typora: New Window"), # Listed but not implemented
+        "new window":
+            # Listed but not implemented
+            R(Key("cs-n"), rdescript="Typora: New Window"),
         # "new tab":      R(Key(""), rdescript="Typora: New Tab"), # Not implemented in Windows OS
         "open file":
             R(Key("c-o"), rdescript="Typora: Open File"),
-        "open [file] quickly":
+        "go [to] file":
             R(Key("c-p"), rdescript="Typora: Open Quickly"),
         "reopen [closed] file":
             R(Key("cs-t"), rdescript="Typora: Reopen Closed File"),
@@ -36,44 +38,40 @@ class TyporaRule(MergeRule):
         "close file":
             R(Key("c-w"), rdescript="Typora: Close"),
         # Edit
-        "new paragraph":
-            R(Key("enter"), rdescript="Typora: New Paragraph") * Repeat(extra="h"),
-        "new line":
+        # "new paragraph": R(Key("enter"), rdescript="Typora: New Paragraph") * Repeat(extra="h"), # Caster: "shock"
+        "new line <h>":
             R(Key("s-enter"), rdescript="Typora: New Line") * Repeat(extra="h"),
         "copy [as] markdown":
             R(Key("cs-c"), rdescript="Typora: Copy as Markdown"),
-        "delete row":
+        "delete row <n>":
             R(Key("cs-backspace"), rdescript="Typora: Delete Row") * Repeat(extra="n"),
         "select [cell | scope]":
             R(Key("c-e"), rdescript="Typora: Select Style Scope or Cell"),
-        "[select word]":
+        "[select] word <n>":
             R(Key("c-d"), rdescript="Typora: Select Word") * Repeat(extra="n"),
-        "delete word":
+        "delete word <n>":
             R(Key("cs-d"), rdescript="Typora: Delete Word") * Repeat(extra="n"),
-        "jump [to] top":
-            R(Key("c-home"), rdescript="Typora: Jump to Top"),
-        "jump [to] selection":
-            R(Key("c-j"), rdescript="Typora: Jump to Selection"),
+        # "jump [to] top":    R(Key("c-home"), rdescript="Typora: Jump to Top"), # Caster: "sauce wally"
+        # "jump [to] selection":  R(Key("c-j"), rdescript="Typora: Jump to Selection"), # Caster: "dunce wally"
         "jump [to] buttom":
             R(Key("c-end"), rdescript="Typora: Jump to Buttom"),
-        # Say "escape" to exit the find/replace context
-        "find":
+        "find":  # Say "escape" to exit the find/replace context
             R(Key("c-f"), rdescript="Typora: Find"),
         "find next":
             R(Key("f3"), rdescript="Typora: Find Next"),
         "replace":
             R(Key("c-h"), rdescript="Typora: Replace"),
         # Paragraph
-        "heading <n>":
+        "heading <h>":
             R(Key("c-%(h)d"), rdescript="Typora: Heading size 0 through 6"),
         "paragraph":
             R(Key("c-o"), rdescript="Typora: Paragraph"),
-        "increase heading [level]":
+        "increase heading [level] <h>":
             R(Key("c-equal"), rdescript="Typora: Increase Heading Level") * Repeat(extra="h"),
-        "decrease heading [level]":
+        "decrease heading [level] <h>":
             R(Key("c-minus"), rdescript="Typora: Decrease Heading Level") * Repeat(extra="h"),
         "table":
-            R(Key("c-t"), rdescript="Typora: Table"), # could be automated.
+            R(Key("c-t"), rdescript="Typora: Table"),  # could be automated.
         "code fences":
             R(Key("cs-k"), rdescript="Typora: Code Fences"),
         "math block":
@@ -82,13 +80,13 @@ class TyporaRule(MergeRule):
             R(Key("cs-q"), rdescript="Typora: Quote"),
         "ordered list":
             R(Key("cs-["), rdescript="Typora: Ordered List"),
-        "indent":
+        "indent <h>":
             R(Key("cs-]"), rdescript="Typora: Indent") * Repeat(extra="h"),
-        "outdent":
+        "out dent <h>":
             R(Key("cs-["), rdescript="Typora: Outdent") * Repeat(extra="h"),
         # Format
         "strong | bold":
-            R(Key("c-b"), rdescript="Typora: Strong"), 
+            R(Key("c-b"), rdescript="Typora: Strong"),
         "emphasis | italicize":
             R(Key("c-i"), rdescript="Typora: Emphasis"),
         "underline":
@@ -122,9 +120,9 @@ class TyporaRule(MergeRule):
             R(Key("f11"), rdescript="Typora: Toggler Fullscreen"),
         "actual size":
             R(Key("cs-0"), rdescript="Typora: Actual Size"),
-        "zoom in":
+        "zoom in <n>":
             R(Key("cs-="), rdescript="Typora: Zoom In") * Repeat(extra="n"),
-        "zoom out":
+        "zoom out <n>":
             R(Key("cs--"), rdescript="Typora: Zoom Out") * Repeat(extra="n"),
         "switch documnets":
             R(Key("c-tab"), rdescript="Typora: Switch Between Opened Documnets"),
@@ -132,7 +130,7 @@ class TyporaRule(MergeRule):
             R(Key("cs-f12"), rdescript="Typora: Toggle DevTools"),
 
     }
-    
+
     extras = [
         Dictation("text"),
         IntegerRefST("h", 0, 6),
@@ -140,8 +138,8 @@ class TyporaRule(MergeRule):
     ]
 
     defaults = {
-    "n": 1,
-    "h": 1
+        "n": 1,
+        "h": 1
     }
 
 
@@ -153,7 +151,6 @@ grammar = Grammar("typora", context=context)
 if settings.SETTINGS["apps"]["typora"]:
     if settings.SETTINGS["miscellaneous"]["rdp_mode"]:
         control.nexus().merger.add_global_rule(TyporaRule())
-        print("added Typora")
     else:
         rule = TyporaRule(name="typora")
         gfilter.run_on(rule)
