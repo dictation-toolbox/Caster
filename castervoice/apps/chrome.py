22c463490e86d3d2186f306f3da1f2d9cb550f17
Alex Boche <akboche@gmail.com>
2019-03-16 23:22:15 -0700

    add new chrome commands

--- castervoice/apps/chrome.py
+++ castervoice/apps/chrome.py
@@ -9,7 +9,7 @@ Command-module for Chrome and Firefox
 """
 #---------------------------------------------------------------------------
 
-from dragonfly import (Grammar, Dictation, Repeat)
+from dragonfly import (Grammar, Context, AppContext, Dictation, Key, Text, Repeat, Function, Choice, Mouse, Pause)
 
 from castervoice.lib import control
 from castervoice.lib import settings
@@ -24,12 +24,36 @@ from castervoice.lib.dfplus.state.short import R
 class ChromeRule(MergeRule):
     pronunciation = "google chrome"
 
-    mapping = { # most keybinds are taken from https://support.google.com/chrome/answer/157179?hl=en
-        "[new] incognito window":       R(Key("cs-n"), rdescript="Browser: New Incognito Window"),
+    mapping = { 
+ 
+    
+ 
+        "new [<n>]":                R(Key("c-t"), rdescript="Browser: New Tab") * Repeat(extra="n"),
+        "new window": Key("c-n"),
+        "(new incognito window | incognito)": Key("cs-n"),
         "new tab [<n>]":                R(Key("c-t"), rdescript="Browser: New Tab") * Repeat(extra="n"),
         "reopen tab [<n>]":             R(Key("cs-t"), rdescript="Browser: Reopen Tab") * Repeat(extra="n"),
+        "close tab [<n>]":               R(Key("c-w")) * Repeat(extra='n'),
         "close all tabs":               R(Key("cs-w"), rdescript="Browser: Close All Tabs"),
-
+        "(nab | next tab) [<n>]":                    R(Key("c-tab")) * Repeat(extra="n"),
+        "(lab | previous tab) [<n>]":                    R(Key("cs-tab")) * Repeat(extra="n"),
+        "(new tab | nab) that": 
+            R(Mouse("middle") + Pause("20") + Key("c-tab"), 
+                rdescript="Browser: when the mouse is hovering over a link open that link in a new tab and then go to that new tab "),
+        
+        "second tab": R(Key("c-2")),
+         "first tab": R(Key("c-1")),
+        "third tab": R(Key("c-3")),
+        "fourth tab": R(Key("c-4")),
+        "fifth tab": R(Key("c-5")),
+        "sixth tab": R(Key("c-6")),
+        "seventh tab": R(Key("c-7")),
+        "eighth tab": R(Key("c-8")),
+        "ninth tab": R(Key("c-9")),
+        "last tab": R(Key("c-1, cs-tab")),
+        "second last tab": R(Key("c-1, cs-tab:2")),
+        
+        
         "go back [<n>]":                R(Key("a-left/20"), rdescript="Browser: Navigate History Backward") * Repeat(extra="n"),
         "go forward [<n>]":             R(Key("a-right/20"), rdescript="Browser: Navigate History Forward") * Repeat(extra="n"),
         "zoom in [<n>]":                R(Key("c-plus/20"), rdescript="Browser: Zoom In") * Repeat(extra="n"),
@@ -42,19 +66,20 @@ class ChromeRule(MergeRule):
         "[toggle] caret browsing":      R(Key("f7"), rdescript="Browser: Caret Browsing"), # now available through an add on, was a standard feature
 
         "home page":                    R(Key("a-home"), rdescript="Browser: Home Page"),
-        "show history":                 R(Key("c-h"), rdescript="Browser: Show History"),
-        "address bar":                  R(Key("c-l"), rdescript="Browser: Address Bar"),
+        "[show] history":                 R(Key("c-h"), rdescript="Browser: Show History"),
+        "[address] bar":                  R(Key("c-l"), rdescript="Browser: Address Bar"),
         "show downloads":               R(Key("c-j"), rdescript="Browser: Show Downloads"),
         "[add] bookmark":               R(Key("c-d"), rdescript="Browser: Add Bookmark"),
         "bookmark all tabs":            R(Key("cs-d"), rdescript="Browser: Bookmark All Tabs"),
         "[toggle] bookmark bar":        R(Key("cs-b"), rdescript="Browser: Toggle Bookmark Bar"),
-        "show bookmarks":               R(Key("cs-o"), rdescript="Browser: Show Bookmarks"),
+        "[show] bookmarks":               R(Key("cs-o"), rdescript="Browser: Show Bookmarks"),
         "switch user":                  R(Key("cs-m"), rdescript="Browser: Switch User"),
         "chrome task manager":          R(Key("s-escape"), rdescript="Browser: Chrome Task Manager"),
         "[toggle] full-screen":         R(Key("f11"), rdescript="Browser: Toggle Fullscreen Mode"),
         "focus notification":           R(Key("a-n"), rdescript="Browser: Focus Notification"),
         "allow notification":           R(Key("as-a"), rdescript="Browser: Allow Notification"),
         "deny notification":            R(Key("as-a"), rdescript="Browser: Deny Notification"),
+
 
         "developer tools":              R(Key("f12"), rdescript="Browser: Developer Tools"),
         "view [page] source":           R(Key("c-u"), rdescript="Browser: View Page Source"),
@@ -64,12 +89,69 @@ class ChromeRule(MergeRule):
         "step out":                     R(Key("s-f11"), rdescript="Browser: Step Out"),
 
         "IRC identify":                 R(Text("/msg NickServ identify PASSWORD"), rdescript="IRC Chat Channel Identify"),
+
+        "google that": R(Key("c-c, c-t, c-v, enter"), rdescript="googles highlighted text"),
+        "duplicate tab":R(Key("a-d,a-c,c-t/15,c-v/15, enter"),
+             rdescript="duplicate the current tab"),
+        "duplicate window":R(Key("a-d,a-c,c-n/15,c-v/15, enter"),
+             rdescript="duplicate the current tab in a new window"),
+        "extensions": R(Key("a-f/20, l, e/15, enter"),
+             rdescript="chrome extensions"),
+        "(menu | three dots)": R(Key("a-f"),
+             rdescript="go to the three dots menu at the top right of chrome"),
+        "settings": R(Key("a-f/5, s"),
+             rdescript="chrome settings"),
+        "downloads": R(Key("c-j"),
+             rdescript="show downloads"),
+        "chrome task manager": R(Key("s-escape"), rdescript="chrome task manager"),
+        "clear browsing data": R(Key("cs-del"), rdescript="clear browsing data"),
+        "developer tools": R(Key("cs-i"), rdescript="developer tools"),
+        "more tools": R(Key("a-f/5, l"), rdescript="more tools"),
+        "more tools": R(Key("a-f/5, l"), rdescript="more tools"),
+
+
+
+# click by voice chrome extension commands
+# these require the click by voice Chrome extension
+# these allow you to browse Google Chrome hands-free
+#  (I haven't tried surfer keys yet, but apparently that's another good option)
+        "<numbers> <dictation>": R(Key("cs-space/30")+Text("%(numbers)d:%(click_by_voice_options)s")
+            + Key("enter/30") + Text("%(dictation)s"), 
+            rdescript="input dictation into numbered text field"),
+        "go <numbers> <dictation>": R(Key("cs-space/30")+Text("%(numbers)d:%(click_by_voice_options)s")
+            + Key("enter/30") + Text("%(dictation)s") + Key("enter"), 
+            rdescript="input dictation into numbered text field then press enter"),
+        "next <numbers> <dictation>": R(Key("cs-space/30")+Text("%(numbers)d:%(click_by_voice_options)s")
+            + Key("enter/30") + Text("%(dictation)s") + Key("tab"), 
+            rdescript="input dictation into numbered text field then press tab"),
+        "<numbers> [<click_by_voice_options>]": R(Key("cs-space/30")
+            + Text("%(numbers)d:%(click_by_voice_options)s") + Key("enter"), 
+            rdescript="click link with click by voice options"),
+        "hide hints": R(Key("cs-space/30")+Text(":-")+Key("enter"),
+             rdescript="hide click by voice hints (i.e. numbers)"),
+        "show hints": R(Key("cs-space/30")+Text(":+")+Key("enter"),
+            rdescript="show click by voice hints (i.e. numbers)"),
+
+
         }
     extras = [
-        Dictation("dict"),
+        Choice("click_by_voice_options", {
+            "go": "f",
+            "click": "c",
+            "push": "b", # open as new tab but don't go to it
+            "tab": "t", # open as new tab and go to it
+            "window": "w",
+            "hover": "h",
+            "link": "k",
+            "copy": "s",
+        }),
+        
+        Dictation("dictation"),
         IntegerRefST("n", 1, 10),
+        IntegerRefST("m", 1, 10),
+        IntegerRefST("numbers", 0, 1000),
     ]
-    defaults = {"n": 1, "dict": "nothing"}
+    defaults = {"n": 1, "dict": "", "click_by_voice_options": "c"}
 
 
 #---------------------------------------------------------------------------
