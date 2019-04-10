e6391e3a0793e47184b24d62a1e140c17d75eec0
Mike Roberts <42875462+mrob95@users.noreply.github.com>
2019-04-10 19:36:13 +0100

    yapf

--- castervoice/apps/adobe_acrobat.py
+++ castervoice/apps/adobe_acrobat.py
@@ -20,85 +20,110 @@ from castervoice.lib.dfplus.merge import gfilter
 from castervoice.lib.dfplus.merge.mergerule import MergeRule
 from castervoice.lib.dfplus.state.short import R
 
-
 ShowHideMenu = Key("a-v, s")
+
+
 class AcrobatRule(MergeRule):
     pronunciation = "acrobat"
 
     mapping = {
-        "[go to] page <n>": R(Key("a-v, n, g/15") + Text("%(n)s") + Key("enter"),
-            rdescript="go to page acrobat)"),
-        "set zoom <n>": R(Key("c-y/40") + Text("%(n)s") + Key("enter"), rdescript="set zoom level"),
+        "[go to] page <n>":
+            R(Key("a-v, n, g/15") + Text("%(n)s") + Key("enter"),
+              rdescript="go to page acrobat)"),
+        "set zoom <n>":
+            R(Key("c-y/40") + Text("%(n)s") + Key("enter"), rdescript="set zoom level"),
+        "open":
+            R(Key("c-o"), rdescript="open"),
+        "duplicate tab":
+            R(Key("a-w,n/40,ws-left"), rdescript="duplicate tab in new window"),
+        "enable scrolling":
+            R(Key("a-v, p, c"),
+              rdescript="enable scrolling as opposed to single page mode"),
+        "(disable scrolling | single page mode)":
+            R(Key("a-v, p, s"),
+              rdescript="single page mode, you can scroll one page at a time"),
+        "(nab | next tab) [<n>]":
+            R(Key("c-tab"), rdescript="next tab")*Repeat(extra="n"),
+        "(lab | prior tab) [<n>]":
+            R(Key("cs-tab"), rdescript="prior tab")*Repeat(extra="n"),
+        "(home button|homer)":
+            R(Mouse("[100, 101], left"), rdescript="click home button on top left"
+              ),  # coordinates may be user dependent
 
-        "open": R(Key("c-o"), rdescript="open"),
-        "duplicate tab":R(Key("a-w,n/40,ws-left"), rdescript="duplicate tab in new window"),
-        "enable scrolling": R(Key("a-v, p, c"), rdescript="enable scrolling as opposed to single page mode"),
-        "(disable scrolling | single page mode)": R(Key("a-v, p, s"),
-             rdescript="single page mode, you can scroll one page at a time"),
-        "(nab | next tab) [<n>]": R(Key("c-tab"),
-             rdescript="next tab") * Repeat(extra="n"),
-        "(lab | prior tab) [<n>]": R(Key("cs-tab"),
-             rdescript="prior tab") * Repeat(extra="n"),
-
-        "(home button|homer)": R(Mouse("[100, 101], left"),
-             rdescript="click home button on top left"), # coordinates may be user dependent
-
- # Sticky Note Commands
-            # must have the cursor over the location where you want the sticky note
-        "add note [<dict>]": R(Mouse("right") + Key("t/5") + Text("%(dict)s"),
-             rdescript="add sticky note with dictation"),
+        # Sticky Note Commands
+        # must have the cursor over the location where you want the sticky note
+        "add note [<dict>]":
+            R(Mouse("right") + Key("t/5") + Text("%(dict)s"),
+              rdescript="add sticky note with dictation"),
         "fast [add] note [<dict>]":
-            R(Mouse("right") + Key("t/5") + Text("%(dict)s") + Pause("10") + Key("escape/5, c-s"),
-             rdescript="add sticky note with dictation then close it and save"),
+            R(Mouse("right") + Key("t/5") + Text("%(dict)s") + Pause("10") +
+              Key("escape/5, c-s"),
+              rdescript="add sticky note with dictation then close it and save"),
         "open blank note":
             R(Mouse("right") + Key("t/5"), rdescript="open sticky note"),
         "add blank note":
             R(Mouse("right") + Key("t/5, escape/5, c-s"),
-                rdescript="open sticky note, close it, and then save"),
+              rdescript="open sticky note, close it, and then save"),
         "delete note":
             R(Mouse("right") + Key("l, c-s"), rdescript="close sticky note then save"),
-
-
-        "(go | go back) [<n>]": R(Key("a-left"),
-             rdescript="go back to previous location") * Repeat(extra='n'),
-        "save as": R(Key("a-f, a"), rdescript="save as"),
+        "(go | go back) [<n>]":
+            R(Key("a-left"), rdescript="go back to previous location")*Repeat(extra='n'),
+        "save as":
+            R(Key("a-f, a"), rdescript="save as"),
 
         # when you open up a document that you have previously saved, and then click save,
-            # Adobe will sometimes make you go back into the save dialogbox
-            # and choose the location you want to save it in and then make you say that you want
-            # to overwrite the file. This is annoying, but this command "fast save" will automatically
-            # do all that for you.
-        "fast save": R(Key("c-s/10, enter/10, enter/10, left, enter"),
-             rdescript="fast save"),
+        # Adobe will sometimes make you go back into the save dialogbox
+        # and choose the location you want to save it in and then make you say that you want
+        # to overwrite the file. This is annoying, but this command "fast save" will automatically
+        # do all that for you.
+        "fast save":
+            R(Key("c-s/10, enter/10, enter/10, left, enter"), rdescript="fast save"),
 
         # if page down goes too far down then try this command
-        "down it [<n>]": R(Key("pgdown:%(n)s, up:4"), rdescript="page down and then go up a little bit"),
-        "up it [<n>]": R(Key("pgup:%(n)s, down:4"), rdescript="page up and then go down a little bit"),
-
-        "tools pane": R(ShowHideMenu + Key("t"), rdescript="tools pane"),
-        "menu bar": R(ShowHideMenu + Key("m"), rdescript="menu bar"),
-        "model tree": R(ShowHideMenu + Key("n, e"),  rdescript="model tree"),
-        "bookmarks": R(ShowHideMenu + Key("n, b"), rdescript="bookmarks"),
-        "[page] thumbnails": R(ShowHideMenu + Key("n, b"), rdescript="page thumbnails"),
-
-        "rotate [<n>]": R(Key("c-plus"), rdescript="rotate page clockwise") * Repeat(extra='n'),
+        "down it [<n>]":
+            R(Key("pgdown:%(n)s, up:4"),
+              rdescript="page down and then go up a little bit"),
+        "up it [<n>]":
+            R(Key("pgup:%(n)s, down:4"),
+              rdescript="page up and then go down a little bit"),
+        "tools pane":
+            R(ShowHideMenu + Key("t"), rdescript="tools pane"),
+        "menu bar":
+            R(ShowHideMenu + Key("m"), rdescript="menu bar"),
+        "model tree":
+            R(ShowHideMenu + Key("n, e"), rdescript="model tree"),
+        "bookmarks":
+            R(ShowHideMenu + Key("n, b"), rdescript="bookmarks"),
+        "[page] thumbnails":
+            R(ShowHideMenu + Key("n, b"), rdescript="page thumbnails"),
+        "rotate [<n>]":
+            R(Key("c-plus"), rdescript="rotate page clockwise")*Repeat(extra='n'),
 
         # Scrolling Commands
-            # Acrobat has a built-in scrolling function with nine speeds.
-            # Unfortunately, there are not separate commands for our scrolling up and down
-            # You have to start by scrolling in the most recently used direction and then reverse the direction
-        "scroll <speed_one_to_nine>": R(Key("cs-h/2, %(speed_one_to_nine)s"), rdescript="scroll <speed_one_to_nine>"),
-        "scroll": R(Key("cs-h/2, 6"), rdescript="scroll"),
-        "change speed <speed_one_to_nine>": R(Key("%(speed_one_to_nine)s, %(speed_one_to_nine)s"), rdescript="change speed <speed_one_to_nine>"),
-        "reverse [direction]": R(Key("minus"), rdescript="reverse scrolling direction while scrolling"),
-        "stop [scrolling]": R(Key("escape"), rdescript="stop scrolling"),
+        # Acrobat has a built-in scrolling function with nine speeds.
+        # Unfortunately, there are not separate commands for our scrolling up and down
+        # You have to start by scrolling in the most recently used direction and then reverse the direction
+        "scroll <speed_one_to_nine>":
+            R(Key("cs-h/2, %(speed_one_to_nine)s"),
+              rdescript="scroll <speed_one_to_nine>"),
+        "scroll":
+            R(Key("cs-h/2, 6"), rdescript="scroll"),
+        "change speed <speed_one_to_nine>":
+            R(Key("%(speed_one_to_nine)s, %(speed_one_to_nine)s"),
+              rdescript="change speed <speed_one_to_nine>"),
+        "reverse [direction]":
+            R(Key("minus"), rdescript="reverse scrolling direction while scrolling"),
+        "stop [scrolling]":
+            R(Key("escape"), rdescript="stop scrolling"),
 
         # cursor commands
-            # (must enable "you single key accelerators to access tools" by going to: edit -> preferences -> general)
-        "highlight": R(Key("u"), rdescript="highlight"),
-        "hand tool": R(Key("h"), rdescript="hand tool"),
-        "select tool": R(Key("v"), rdescript="select tool"),
-
+        # (must enable "you single key accelerators to access tools" by going to: edit -> preferences -> general)
+        "highlight":
+            R(Key("u"), rdescript="highlight"),
+        "hand tool":
+            R(Key("h"), rdescript="hand tool"),
+        "select tool":
+            R(Key("v"), rdescript="select tool"),
     }
     extras = [
         Dictation("dict"),
