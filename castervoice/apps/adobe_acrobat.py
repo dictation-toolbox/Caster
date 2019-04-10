72d5d00ec458a884f26379001a76d32b737b9f5f
Mike Roberts <42875462+mrob95@users.noreply.github.com>
2019-04-10 19:35:37 +0100

    Fixed rdescripts

--- castervoice/apps/adobe_acrobat.py
+++ castervoice/apps/adobe_acrobat.py
@@ -26,12 +26,10 @@ class AcrobatRule(MergeRule):
     pronunciation = "acrobat"
 
     mapping = {
-
-        
         "[go to] page <n>": R(Key("a-v, n, g/15") + Text("%(n)s") + Key("enter"),
             rdescript="go to page acrobat)"),
         "set zoom <n>": R(Key("c-y/40") + Text("%(n)s") + Key("enter"), rdescript="set zoom level"),
-        
+
         "open": R(Key("c-o"), rdescript="open"),
         "duplicate tab":R(Key("a-w,n/40,ws-left"), rdescript="duplicate tab in new window"),
         "enable scrolling": R(Key("a-v, p, c"), rdescript="enable scrolling as opposed to single page mode"),
@@ -39,9 +37,9 @@ class AcrobatRule(MergeRule):
              rdescript="single page mode, you can scroll one page at a time"),
         "(nab | next tab) [<n>]": R(Key("c-tab"),
              rdescript="next tab") * Repeat(extra="n"),
-        "(lab | prior tab) [<n>]": R(Key("cs-tab")),
+        "(lab | prior tab) [<n>]": R(Key("cs-tab"),
              rdescript="prior tab") * Repeat(extra="n"),
-        
+
         "(home button|homer)": R(Mouse("[100, 101], left"),
              rdescript="click home button on top left"), # coordinates may be user dependent
 
@@ -59,16 +57,16 @@ class AcrobatRule(MergeRule):
                 rdescript="open sticky note, close it, and then save"),
         "delete note":
             R(Mouse("right") + Key("l, c-s"), rdescript="close sticky note then save"),
-              
-        
-        "(go | go back) [<n>]": Key("a-left"),
+
+
+        "(go | go back) [<n>]": R(Key("a-left"),
              rdescript="go back to previous location") * Repeat(extra='n'),
         "save as": R(Key("a-f, a"), rdescript="save as"),
 
         # when you open up a document that you have previously saved, and then click save,
             # Adobe will sometimes make you go back into the save dialogbox
-            # and choose the location you want to save it in and then make you say that you want 
-            # to overwrite the file. This is annoying, but this command "fast save" will automatically 
+            # and choose the location you want to save it in and then make you say that you want
+            # to overwrite the file. This is annoying, but this command "fast save" will automatically
             # do all that for you.
         "fast save": R(Key("c-s/10, enter/10, enter/10, left, enter"),
              rdescript="fast save"),
@@ -82,24 +80,24 @@ class AcrobatRule(MergeRule):
         "model tree": R(ShowHideMenu + Key("n, e"),  rdescript="model tree"),
         "bookmarks": R(ShowHideMenu + Key("n, b"), rdescript="bookmarks"),
         "[page] thumbnails": R(ShowHideMenu + Key("n, b"), rdescript="page thumbnails"),
-               
-        "rotate [<n>]": R(Key("c-plus"),  rdescript="rotate page clockwise" * Repeat(extra='n'),
+
+        "rotate [<n>]": R(Key("c-plus"), rdescript="rotate page clockwise") * Repeat(extra='n'),
 
         # Scrolling Commands
-            # Acrobat has a built-in scrolling function with nine speeds. 
+            # Acrobat has a built-in scrolling function with nine speeds.
             # Unfortunately, there are not separate commands for our scrolling up and down
             # You have to start by scrolling in the most recently used direction and then reverse the direction
-        "scroll <speed_one_to_nine>": Key("cs-h/2, %(speed_one_to_nine)s"),
-        "scroll": Key("cs-h/2, 6"),
-        "change speed <speed_one_to_nine>": Key("%(speed_one_to_nine)s, %(speed_one_to_nine)s"),
+        "scroll <speed_one_to_nine>": R(Key("cs-h/2, %(speed_one_to_nine)s"), rdescript="scroll <speed_one_to_nine>"),
+        "scroll": R(Key("cs-h/2, 6"), rdescript="scroll"),
+        "change speed <speed_one_to_nine>": R(Key("%(speed_one_to_nine)s, %(speed_one_to_nine)s"), rdescript="change speed <speed_one_to_nine>"),
         "reverse [direction]": R(Key("minus"), rdescript="reverse scrolling direction while scrolling"),
         "stop [scrolling]": R(Key("escape"), rdescript="stop scrolling"),
 
-        # cursor commands 
+        # cursor commands
             # (must enable "you single key accelerators to access tools" by going to: edit -> preferences -> general)
-        "highlight": R(Key("u"), rdescript=""),
-        "hand tool": R(Key("h"), rdescript=""),
-        "select tool": R(Key("v"), rdescript=""),
+        "highlight": R(Key("u"), rdescript="highlight"),
+        "hand tool": R(Key("h"), rdescript="hand tool"),
+        "select tool": R(Key("v"), rdescript="select tool"),
 
     }
     extras = [
