da3219a53653e521e700e09f3ace499e8f57cf9f
Alex Boche <akboche@gmail.com>
2019-04-19 13:00:01 -0700

    use text action instead of paste in dialogue.py

--- castervoice/apps/file_dialogue.py
+++ castervoice/apps/file_dialogue.py
@@ -1,4 +1,4 @@
-import pyperclip
+# import pyperclip
 from dragonfly import (Grammar, MappingRule, Dictation, IntegerRef,
                        Repeat, Pause, Function, Choice, AppContext)
 
@@ -26,17 +26,17 @@ if not CONFIG:
     
 
 def dialogue_bring_it(folder_path):
-    Key("c-l/20").execute()
-    # Attempt to paste enclosed text without altering clipboard
-    if not paste_string_without_altering_clipboard(folder_path):
-        print("failed to paste {}".format(folder_path))
-    # the paste without altering the clipboard seems a bit inconsistent for me
-    # if it's not working properly, here's an alternative method (commented out) that unfortunately does alter the clipboard
+    Key("c-l/5").execute()
+    Text("{}".format(folder_path)).execute()
+    # Alternate method: is faster but somewhat inconsistent.
+        # if not paste_string_without_altering_clipboard(folder_path):
+        #     print("failed to paste {}".format(folder_path))
+        # Pause("10").execute()
+    # Second alternate method:is fast and consistent, but alters the clipboard
         # pyperclip.copy(folder_path)
         # Pause("5").execute()
         # Key("c-v/30").execute()
-    Pause("20").execute()
-    Key("enter/10, a-d/10, tab:4").execute() 
+    Key("enter/20, a-d/5, tab:4").execute() 
 
 
 class FileDialogueRule(MergeRule):
