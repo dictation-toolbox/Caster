3be35315db88c4e2518a46e19a198e8134068ebb
Alex Boche <akboche@gmail.com>
2019-04-18 20:41:08 -0700

    file_dialogue.py is working, the bringme commands therein are slightly inconsistent

--- castervoice/apps/file_dialogue.py
+++ castervoice/apps/file_dialogue.py
@@ -1,16 +1,18 @@
-# import pyperclip
-from dragonfly import (AppContext, Dictation, Grammar, IntegerRef, Key, MappingRule,
-                       Pause, Repeat, Text)
-from dragonfly.actions.action_mimic import Mimic
+import pyperclip
+from dragonfly import (Grammar, MappingRule, Dictation, IntegerRef,
+                       Repeat, Pause, Function, Choice, AppContext)
 
 from castervoice.lib import control, context, utilities, settings
+from castervoice.lib.context import paste_string_without_altering_clipboard
+from castervoice.lib import settings
 from castervoice.lib.dfplus.additions import IntegerRefST
 from castervoice.lib.dfplus.merge import gfilter
 from castervoice.lib.dfplus.merge.mergerule import MergeRule
 from castervoice.lib.dfplus.state.short import R
+from castervoice.lib.actions import (Key, Text)
 
 
-# note that the tab structure of of Windows Explorer window is slightly different than 
+# note that the tab structure of Windows Explorer window is slightly different than 
 # that of Windows Explorer dialogbox (aka child window)
 # this file is only for Windows Explorer dialogbox.
 
@@ -26,15 +28,15 @@ if not CONFIG:
 def dialogue_bring_it(folder_path):
     Key("c-l/20").execute()
     # Attempt to paste enclosed text without altering clipboard
-    if not context.paste_string_without_altering_clipboard(folder_path):
+    if not paste_string_without_altering_clipboard(folder_path):
         print("failed to paste {}".format(folder_path))
     # the paste without altering the clipboard seems a bit inconsistent for me
-    # if it's not working properly, here's an alternative method that does alter the clipboard
+    # if it's not working properly, here's an alternative method (commented out) that unfortunately does alter the clipboard
         # pyperclip.copy(folder_path)
         # Pause("5").execute()
         # Key("c-v/30").execute()
-    Pause("10").execute()
-    Key("enter/10, tab:4").execute() 
+    Pause("20").execute()
+    Key("enter/10, a-d/10, tab:4").execute() 
 
 
 class FileDialogueRule(MergeRule):
