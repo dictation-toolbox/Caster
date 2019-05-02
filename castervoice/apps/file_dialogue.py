b58f3aed9a7ac35d0ad6169b4ed2edc8695a3ba4
Alex Boche <akboche@gmail.com>
2019-05-01 20:02:36 -0700

    fix Windows Explorer file dialogue navigation commands

--- castervoice/apps/file_dialogue.py
+++ castervoice/apps/file_dialogue.py
@@ -1,8 +1,8 @@
-from dragonfly import (AppContext, Dictation, Grammar, IntegerRef, Key, MappingRule,
-                       Pause, Repeat, Text)
-from dragonfly.actions.action_mimic import Mimic
+from dragonfly import (Dictation, Grammar, IntegerRef, Key, MappingRule, Pause, Repeat,
+                       Text)
 
 from castervoice.lib import control, settings
+from castervoice.lib.context import AppContext
 from castervoice.lib.dfplus.additions import IntegerRefST
 from castervoice.lib.dfplus.merge import gfilter
 from castervoice.lib.dfplus.merge.mergerule import MergeRule
@@ -20,12 +20,22 @@ class FileDialogueRule(MergeRule):
         "forward [<n>]":
             R(Key("a-right"), rdescript="File Dialogue: Navigate forward")*
             Repeat(extra="n"),
-        "(files | file list)":
-            R(Key("a-d, f6:3"), rdescript="File Dialogue: Files list"),
-        "navigation [pane]":
-            R(Key("a-d, f6:2"), rdescript="File Dialogue: Navigation pane"),
+        "search [<text>]":
+            R(Key("c-l, tab") + Text("%(text)s"), rdescript="File Dialogue: Search"),
+        "organize":
+            R(Key("c-l, tab:2"), rdescript="File Dialogue: press 'organize' button"),
+        "(left | navigation) pane":
+            R(Key("c-l, tab:3"), rdescript="File dialogue: navigation pane"),
+        "(center|file|files|folder) (list | pane)":
+            R(Key("c-l, tab:4"), rdescript="File Dialogue: file list"),
+        "files":
+            R(Key("c-l, tab:4"), rdescript="File Dialogue: files list"),  # same as above
+        "sort [headings]":
+            R(Key("c-l, tab:5"), rdescript="File Dialogue: sort"),
         "[file] name":
-            R(Key("a-d, f6:5"), rdescript="File Dialogue: File name"),
+            R(Key("c-l, tab:6"), rdescript="File Dialogue: filename"),
+        "file type":
+            R(Key("c-l, tab:7"), rdescript="File Dialogue: file type"),
     }
     extras = [IntegerRefST("n", 1, 10)]
     defaults = {
