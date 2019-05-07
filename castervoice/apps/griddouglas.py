d983e3ec900885de7459e1f602da3fb62abdf15b
seekm <seekm@users.noreply.github.com>
2019-05-07 12:17:41 +0200

    Add select feature

--- castervoice/apps/griddouglas.py
+++ castervoice/apps/griddouglas.py
@@ -2,7 +2,7 @@
 Command-module for DouglasGrid
 
 """
-from dragonfly import (Grammar, Function, Playback, Choice, MappingRule)
+from dragonfly import (Grammar, Function, Playback, Choice, MappingRule, Mouse)
 
 from castervoice.asynch.mouse import grids
 from castervoice.lib import control
@@ -13,46 +13,104 @@ from castervoice.lib.dfplus.merge.mergerule import MergeRule
 from castervoice.lib.dfplus.state.short import R
 from castervoice.lib.context import AppContext
 
+import win32api, win32con, time
 
 _NEXUS = control.nexus()
-
 
 def kill(nexus):
     nexus.comm.get_com("grids").kill()
 
-
-def send_input(n, n2, action, nexus):
+def send_input(x, y, action, nexus):
     s = nexus.comm.get_com("grids")
-    s.move_mouse(int(n), int(n2))
+    s.move_mouse(int(x), int(y))
+    int_a = int(action)
+    if (int_a == 0) | (int_a == 1):
+        s.kill()
+        grids.wait_for_death(settings.DOUGLAS_TITLE)
+    if int_a == 0:
+        Mouse("left").execute()
+    elif int_a == 1:
+        Mouse("right").execute()
+
+def send_input_select(x1, y1, x2, y2, nexus):
+    s = nexus.comm.get_com("grids")
+    s.move_mouse(int(x1), int(y1))
+    _x1, _y1 = win32api.GetCursorPos()
+    s.move_mouse(int(x2), int(y2))
+    _x2, _y2 = win32api.GetCursorPos()
     s.kill()
     grids.wait_for_death(settings.DOUGLAS_TITLE)
-    int_a = int(action)
-    if int_a == 0:
-        Playback([(["mouse", "left", "click"], 0.0)]).execute()
-    elif int_a == 1:
-        Playback([(["mouse", "right", "click"], 0.0)]).execute()
+    drag_from_to(_x1,_y1,_x2,_y2)
 
+def send_input_select_short(x1, y1, x2, nexus):
+    send_input_select(x1, y1, x2, y1, nexus)
+
+def drag_from_to(x1, y1, x2, y2):
+    win32api.SetCursorPos((x1,y1))
+    time.sleep(0.1)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    win32api.SetCursorPos((x2,y2))
+    time.sleep(0.1)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+
+x1 = None
+x2 = None
+y1 = None
+y2 = None
+
+def store_point(point):
+    global x1, y1, x2, y2
+    int_p = int(point)
+    if int_p == 1:
+        x1, y1 = win32api.GetCursorPos()
+    elif int_p == 2:
+        x2, y2 = win32api.GetCursorPos()
+    else:
+        raise Exception("Illegal point specified")
+
+def select_text(nexus):
+    global x1, y1, x2, y2
+    s = nexus.comm.get_com("grids")
+    s.kill()
+    grids.wait_for_death(settings.DOUGLAS_TITLE)
+    drag_from_to(x1,y1,x2,y2)
 
 class GridControlRule(MergeRule):
 
     mapping = {
-        "<n> [by] <n2> [<action>]":
+        "<x> [by] <y> [<action>]":
             R(Function(send_input, nexus=_NEXUS), rdescript="Douglas Grid: Action"),
+        "<x1> [by] <y1> select <x2> [by] <y2>":
+            R(Function(send_input_select, nexus=_NEXUS), rdescript="Douglas Grid: Select (long version)"),
+        "<x1> [by] <y1> select <x2>":
+            R(Function(send_input_select_short, nexus=_NEXUS), rdescript="Douglas Grid: Select (short version)"),
+        "point <point>":
+            R(Function(store_point), rdescript="Douglas Grid: Stor point"),
+        "select":
+            R(Function(select_text, nexus=_NEXUS), rdescript="Douglas Grid: Select"),
         "exit | escape | cancel":
-            R(Function(kill, nexus=_NEXUS), rdescript="Exit Douglas Grid"),
+            R(Function(kill, nexus=_NEXUS), rdescript="Douglas Grid: Exit"),
     }
     extras = [
-        IntegerRefST("n", 0, 300),
-        IntegerRefST("n2", 0, 300),
+        IntegerRefST("x", 0, 300),
+        IntegerRefST("y", 0, 300),
+        IntegerRefST("x1", 0, 300),
+        IntegerRefST("y1", 0, 300),
+        IntegerRefST("x2", 0, 300),
+        IntegerRefST("y2", 0, 300),
         Choice("action", {
             "kick": 0,
             "psychic": 1,
+            "move": 2,
+        }),
+        Choice("point", {
+            "one": 1,
+            "two": 2,
         }),
     ]
     defaults = {
-        "action": -1,
+        "action": 0,
     }
-
 
 #---------------------------------------------------------------------------
 
--- castervoice/apps/gridrainbow.py
+++ castervoice/apps/gridrainbow.py
@@ -3,7 +3,7 @@ Command-module for RainbowGrid
 
 """
 
-from dragonfly import (Grammar, Function, Playback, Choice, MappingRule)
+from dragonfly import (Grammar, Function, Playback, Choice, MappingRule, Mouse)
 
 from castervoice.asynch.mouse import grids
 from castervoice.lib import control
@@ -14,60 +14,140 @@ from castervoice.lib.dfplus.merge.mergerule import MergeRule
 from castervoice.lib.dfplus.state.short import R
 from castervoice.lib.context import AppContext
 
-_NEXUS = control.nexus()
+import win32api, win32con, time
 
+_NEXUS = control.nexus()
 
 def kill(nexus):
     nexus.comm.get_com("grids").kill()
 
-
 def send_input(pre, color, n, action, nexus):
     s = nexus.comm.get_com("grids")
     s.move_mouse(int(pre), int(color), int(n))
-    s.kill()
-    grids.wait_for_death(settings.RAINBOW_TITLE)
     int_a = int(action)
+    if (int_a == 0) | (int_a == 1):
+        s.kill()
+        grids.wait_for_death(settings.DOUGLAS_TITLE)
     if int_a == 0:
-        Playback([(["mouse", "left", "click"], 0.0)]).execute()
+        Mouse("left").execute()
     elif int_a == 1:
-        Playback([(["mouse", "right", "click"], 0.0)]).execute()
+        Mouse("right").execute()
 
+def send_input_select(pre1, color1, n1, pre2, color2, n2, nexus):
+    s = nexus.comm.get_com("grids")
+    s.move_mouse(int(pre1), int(color1), int(n1))
+    _x1, _y1 = win32api.GetCursorPos()
+    s.move_mouse(int(pre2), int(color2), int(n2))
+    _x2, _y2 = win32api.GetCursorPos()
+    s.kill()
+    grids.wait_for_death(settings.DOUGLAS_TITLE)
+    drag_from_to(_x1,_y1,_x2,_y2)
+
+def send_input_select_short(pre1, color1, n1, n2, nexus):
+    send_input_select(pre1, color1, n1, pre1, color1, n2, nexus)
+
+def drag_from_to(x1, y1, x2, y2):
+    win32api.SetCursorPos((x1,y1))
+    time.sleep(0.1)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    win32api.SetCursorPos((x2,y2))
+    time.sleep(0.1)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+
+x1 = None
+x2 = None
+y1 = None
+y2 = None
+
+def store_point(point):
+    global x1, y1, x2, y2
+    int_p = int(point)
+    if int_p == 1:
+        x1, y1 = win32api.GetCursorPos()
+    elif int_p == 2:
+        x2, y2 = win32api.GetCursorPos()
+    else:
+        raise Exception("Illegal point specified")
+
+def select_text(nexus):
+    global x1, y1, x2, y2
+    s = nexus.comm.get_com("grids")
+    s.kill()
+    grids.wait_for_death(settings.DOUGLAS_TITLE)
+    drag_from_to(x1,y1,x2,y2)
 
 class GridControlRule(MergeRule):
 
     mapping = {
         "[<pre>] <color> <n> [<action>]":
-            R(Function(send_input, nexus=_NEXUS), rdescript="Rainbow: Rainbow Grid: Action"),
+            R(Function(send_input, nexus=_NEXUS), rdescript="Rainbow Grid: Action"),
+        "[<pre1>] <color1> <n1> select [<pre2>] <color2> <n2>":
+            R(Function(send_input_select, nexus=_NEXUS), rdescript="Rainbow Grid: Select (long version)"),
+        "[<pre1>] <color1> <n1> select <n2>":
+            R(Function(send_input_select_short, nexus=_NEXUS), rdescript="Rainbow Grid: Select (short version)"),
+        "point <point>":
+            R(Function(store_point), rdescript="Rainbow Grid: Store point"),
+        "select":
+            R(Function(select_text, nexus=_NEXUS), rdescript="Rainbow Grid: Select"),
         "exit | escape | cancel":
-            R(Function(kill, nexus=_NEXUS), rdescript="Rainbow: Exit Rainbow Grid"),
+            R(Function(kill, nexus=_NEXUS), rdescript="Rainbow Grid: Exit"),
     }
     extras = [
         IntegerRefST("pre", 0, 9),
+        IntegerRefST("pre1", 0, 9),
+        IntegerRefST("pre2", 0, 9),
         Choice(
             "color", {
-                "red": 0,
-                "(orange | tan | brown)": 1,
-                "yellow": 2,
-                "green": 3,
-                "blue": 4,
-                "purple": 5
+                "(red | rot)": 0,
+                "(orange | tan | brown | braun)": 1,
+                "(yellow | gelb)": 2,
+                "(green | gruen)": 3,
+                "(blue | blau)": 4,
+                "(purple | lila)": 5
             }),
+        Choice(
+            "color1", {
+                "(red | rot)": 0,
+                "(orange | tan | brown | braun)": 1,
+                "(yellow | gelb)": 2,
+                "(green | gruen)": 3,
+                "(blue | blau)": 4,
+                "(purple | lila)": 5
+            }),
+        Choice(
+            "color2", {
+                "(red | rot)": 0,
+                "(orange | tan | brown | braun)": 1,
+                "(yellow | gelb)": 2,
+                "(green | gruen)": 3,
+                "(blue | blau)": 4,
+                "(purple | lila)": 5
+            }),
+        IntegerRefST("n", 0, 100),
+        IntegerRefST("n1", 0, 100),
+        IntegerRefST("n2", 0, 100),
         Choice("action", {
             "kick": 0,
             "psychic": 1,
+            "move": 2,
         }),
-        IntegerRefST("n", 0, 100),
+        Choice("point", {
+            "one": 1,
+            "two": 2,
+        }),
     ]
     defaults = {
         "pre": 0,
-        "action": -1,
+        "pre1": 0,
+        "pre2": 0,
+        "action": 0,
     }
-
 
 #---------------------------------------------------------------------------
 
 context = AppContext(title="rainbowgrid")
 grammar = Grammar("rainbowgrid", context=context)
+
 if settings.SETTINGS["apps"]["rainbow"]:
     rule = GridControlRule(name="rainbow")
     gfilter.run_on(rule)
