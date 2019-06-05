b29b7e2bfe09eeebd011e5857b219c3cc4140e26
Kendon Bell <kmb56@berkeley.edu>
2019-06-05 12:13:28 -0700

    add separate backslash command

--- castervoice/lib/ccr/core/punctuation.py
+++ castervoice/lib/ccr/core/punctuation.py
@@ -22,7 +22,7 @@ text_punc_dict = {
     "plus":                                 "+",
     "comma":                                ",",  
     "minus":                                "-",
-    "period | dot":                       ".", 
+    "period | dot":                         ".", 
     "slash":                                "/",
     "deckle":                               ":",
     "semper":                               ";",
@@ -62,6 +62,9 @@ class Punctuation(MergeRule):
     mapping = {
         "[<long>] <text_punc> [<npunc>]": 
             R(Text("%(long)s" + "%(text_punc)s" + "%(long)s"))*Repeat(extra="npunc"),
+        # For some reason, this one doesn't work through the other function
+        "[<long>] backslash [<npunc>]":
+            R(Text("%(long)s" + "\\" + "%(long)s"), rdescript="Core: Back Slash")*Repeat(extra="npunc"),            
         "<double_text_punc>": 
             R(Text("%(double_text_punc)s") + Key("left")),
         "tabby [<npunc>]":
