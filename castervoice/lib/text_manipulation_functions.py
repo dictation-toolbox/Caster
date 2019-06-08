18cf74ea1b3a2d30325088673c6a8130f241ebce
Kendon Bell <kendonB@users.noreply.github.com>
2019-06-09 08:25:00 +1200

    add import copy2

--- castervoice/lib/text_manipulation_functions.py
+++ castervoice/lib/text_manipulation_functions.py
@@ -1,6 +1,7 @@
 from dragonfly import Key, Pause, AppContext, Window
 import pyperclip
 import re 
+import copy
 from castervoice.lib import context
 from castervoice.lib.ccr.core.punctuation import text_punc_dict,  double_text_punc_dict
 from castervoice.lib.alphanumeric import caster_alphabet
