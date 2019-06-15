f85198c02d3745e6db49edcd118531d2a32f8218
Kendon Bell <kmbell56@gmail.com>
2019-06-15 16:07:03 +1200

    tidy message

--- castervoice/lib/ccr/core/text_manipulation.py
+++ castervoice/lib/ccr/core/text_manipulation.py
@@ -15,13 +15,15 @@ from castervoice.lib.ccr.core.punctuation import text_punc_dict,  double_text_pu
 from castervoice.lib.alphanumeric import caster_alphabet
 
 # Advertisement
-print("""Check out the new experimental text manipulation commands in castervoice\lib\ccr\core\\text_manipulation.py 
-    where you can disable this message. Enable them by saying "enable text manipulation". You may want to reduce the pause or sleep time
-    in the functions text_manipulation_copy and text_manipulation_paste in castervoice\lib\\text_manipulation_functions.py
-    These are WIP, Please give feedback and report bugs""")
-
-
-
+print("""Text Manipulation: Check out the new experimental text 
+    manipulation commands at https://caster.readthedocs.io/en/latest/readthedocs/TextManipulation 
+    You can delete this message in castervoice\lib\ccr\core\\text_manipulation.py. 
+    Enable these commands by saying "enable text manipulation". 
+    You may want to reduce the pause or sleep time in the 
+    functions text_manipulation_copy and text_manipulation_paste 
+    in castervoice\lib\\text_manipulation_functions.py 
+    These are a work in progress; please give feedback and 
+    report bugs on the Caster github page.""")
 
 """ requires a recent version of dragonfly because of recent modification of the Function action
     # I think dragonfly2-0.13.0
