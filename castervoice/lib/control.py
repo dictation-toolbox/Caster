4b5790e8391971575dceb127fe55d2e61e03d765
Mike Roberts <42875462+mrob95@users.noreply.github.com>
2019-06-23 21:11:16 +0100

    rule loading commands

--- castervoice/lib/control.py
+++ castervoice/lib/control.py
@@ -1,10 +1,36 @@
+from dragonfly import Grammar
+
 from castervoice.lib.ctrl.nexus import Nexus
+from castervoice.lib import settings
+from castervoice.lib.dfplus.merge import gfilter
 
 _NEXUS = None
-
 
 def nexus():
     global _NEXUS
     if _NEXUS is None:
         _NEXUS = Nexus()
     return _NEXUS
+
+
+'''
+Commands for easily loading different types of rules, e.g.:
+control.non_ccr_app_rule(FirefoxRule(), AppContext("firefox"))
+'''
+def non_ccr_app_rule(rule, context=None):
+    grammar = Grammar(context=context)
+    if settings.SETTINGS["miscellaneous"]["rdp_mode"]:
+        nexus().merger.add_global_rule(rule)
+    else:
+        gfilter.run_on(rule)
+        grammar.add_rule(rule)
+        grammar.load()
+
+def ccr_app_rule(rule, context=None):
+    nexus().merger.add_app_rule(rule, context=context)
+
+def global_rule(rule):
+    nexus().merger.add_global_rule(rule)
+
+def selfmod_rule(rule):
+    nexus().merger.add_selfmod_rule(rule)
