82ff1f0979d096842f70983192aee342799ddf1e
Mike Roberts <42875462+mrob95@users.noreply.github.com>
2019-06-26 16:57:54 +0100

    Move methods into BringRule

--- castervoice/lib/ccr/recording/bringme.py
+++ castervoice/lib/ccr/recording/bringme.py
@@ -1,119 +1,24 @@
 from castervoice.lib.imports import *
 
-if os.path.isfile(settings.SETTINGS["paths"]["BRINGME_PATH"]) is False:
-    bringme_default = settings.SETTINGS["paths"]["BRINGME_DEFAULTS_PATH"]
-    bringme_user = settings.SETTINGS["paths"]["BRINGME_PATH"]
-    shutil.copy(bringme_default, bringme_user)
-
-CONFIG = utilities.load_toml_file(settings.SETTINGS["paths"]["BRINGME_PATH"])
-if not CONFIG:
-    # logger.warn("Could not load bringme defaults")
-    print("Could not load bringme defaults")
-
-
-def refresh():
-    bring_rule.refresh()
-
-
-# module functions
-def bring_it(desired_item):
-    '''
-    Currently simply invoke os.startfile. New thread keeps Dragon from crashing.
-    '''
-    item, item_type = desired_item
-    if item_type == "website":
-        browser = utilities.default_browser_command()
-        subprocess.Popen(shlex.split(browser.replace('%1', item)))
-    elif item_type == 'folder':
-        subprocess.Popen([r'C:\Windows\explorer.exe', item])
-    elif item_type == 'program':
-        subprocess.Popen(item)
-    else:
-        threading.Thread(target=os.startfile, args=(item, )).start()
-
-
-def bring_add(launch, key):
-    '''
-    Add current program or highlighted text to bring me
-    '''
-    key = str(key)
-    if launch == "program":
-        path = utilities.get_active_window_path()
-        if not path:
-            # dragonfly.get_engine().speak("program not detected")
-            print("Program path for bring me not found ")
-    # elif launch == 'file':
-    # no way to add file via pyperclip
-    else:
-        Key("a-d/5").execute()
-        fail, path = context.read_selected_without_altering_clipboard()
-        if fail == 2:
-            # FIXME
-            time.sleep(0.1)
-            _, path = context.read_selected_without_altering_clipboard()
-            if not path:
-                # dragonfly.get_engine().speak("nothing selected")
-                print("Selection for bring me not found ")
-        Key("escape").execute()
-    if not path:
-        # logger.warn('Cannot add %s as %s to bringme: cannot get path', launch, key)
-        return
-    CONFIG[launch][key] = path
-    utilities.save_toml_file(CONFIG, settings.SETTINGS["paths"]["BRINGME_PATH"])
-    refresh()
-
-
-def bring_remove(key):
-    '''
-    Remove item from bring me
-    '''
-    key = str(key)
-    for section in CONFIG.keys():
-        if key in CONFIG[section]:
-            del CONFIG[section][key]
-            utilities.save_toml_file(CONFIG, settings.SETTINGS["paths"]["BRINGME_PATH"])
-            refresh()
-            return
-
-
-def bring_restore():
-    '''
-    Restore bring me list to defaults
-    '''
-    global CONFIG
-    CONFIG = utilities.load_toml_file(settings.SETTINGS["paths"]["BRINGME_DEFAULTS_PATH"])
-    refresh()
-
-
-def _rebuild_items():
-    # logger.debug('Bring me rebuilding extras')
-    return {
-        key: (os.path.expandvars(value), header)
-        for header, section in CONFIG.iteritems() for key, value in section.iteritems()
-    }
-
-
 class BringRule(SelfModifyingRule):
     pronunciation = "bring me"
 
     def refresh(self, *args):
-        # logger.debug('Bring me refresh')
-        self.extras[0] = Choice('desired_item', _rebuild_items())
-        self.reset(self.mapping)
-
-    mapping = {
-        "bring me <desired_item>":
-            R(Function(bring_it)),
-        "<launch> to bring me as <key>":
-            R(Function(bring_add, extra={"launch", "key"})),
-        "remove <key> from bring me":
-            R(Function(bring_remove, extra="key")),
-        "restore bring me defaults":
-            R(Function(bring_restore)),
-    }
+        mapping = {
+            "bring me <desired_item>":
+                R(Function(self.bring_it)),
+            "<launch> to bring me as <key>":
+                R(Function(self.bring_add, extra={"launch", "key"})),
+            "remove <key> from bring me":
+                R(Function(self.bring_remove, extra="key")),
+            "restore bring me defaults":
+                R(Function(self.bring_restore)),
+        }
+        self.extras[0] = Choice('desired_item', self._rebuild_items())
+        self.reset(mapping)
 
     extras = [
-        Choice("desired_item", _rebuild_items()),
+        Choice("desired_item", {}),
         Choice(
             "launch", {
                 "[current] program": "program",
@@ -126,6 +31,94 @@ class BringRule(SelfModifyingRule):
 
     defaults = {'desired_item': ('', ""), 'launch': 'program', 'key': ''}
 
-bring_rule = BringRule()
+    def __init__(self):
 
-control.non_ccr_app_rule(bring_rule, context=None, rdp=False)
+        self.config_path = settings.SETTINGS["paths"]["BRINGME_PATH"]
+        self.defaults_path = settings.SETTINGS["paths"]["BRINGME_DEFAULTS_PATH"]
+        self.config = {}
+        self.load_config()
+
+        SelfModifyingRule.__init__(self)
+        self.refresh()
+
+    # module functions
+    def bring_it(self, desired_item):
+        '''
+        Currently simply invoke os.startfile. New thread keeps Dragon from crashing.
+        '''
+        item, item_type = desired_item
+        if item_type == "website":
+            browser = utilities.default_browser_command()
+            subprocess.Popen(shlex.split(browser.replace('%1', item)))
+        elif item_type == 'folder':
+            subprocess.Popen([r'C:\Windows\explorer.exe', item])
+        elif item_type == 'program':
+            subprocess.Popen(item)
+        else:
+            threading.Thread(target=os.startfile, args=(item, )).start()
+
+    def bring_add(self, launch, key):
+        # Add current program or highlighted text to bring me
+        key = str(key)
+        if launch == "program":
+            path = utilities.get_active_window_path()
+            if not path:
+                # dragonfly.get_engine().speak("program not detected")
+                print("Program path for bring me not found ")
+        # elif launch == 'file':
+        # no way to add file via pyperclip
+        else:
+            Key("a-d/5").execute()
+            fail, path = context.read_selected_without_altering_clipboard()
+            if fail == 2:
+                # FIXME
+                time.sleep(0.1)
+                _, path = context.read_selected_without_altering_clipboard()
+                if not path:
+                    # dragonfly.get_engine().speak("nothing selected")
+                    print("Selection for bring me not found ")
+            Key("escape").execute()
+        if not path:
+            # logger.warn('Cannot add %s as %s to bringme: cannot get path', launch, key)
+            return
+        self.config[launch][key] = path
+        self.save_config()
+        self.refresh()
+
+    def bring_remove(self, key):
+        # Remove item from bring me
+        key = str(key)
+        for section in self.config.keys():
+            if key in self.config[section]:
+                del self.config[section][key]
+                self.save_config()
+                self.refresh()
+                return
+
+    def bring_restore(self):
+        # Restore bring me list to defaults
+        self.config = utilities.load_toml_file(self.defaults_path)
+        self.refresh()
+
+    def _rebuild_items(self):
+        # logger.debug('Bring me rebuilding extras')
+        return {
+            key: (os.path.expandvars(value), header)
+            for header, section in self.config.iteritems()
+            for key, value in section.iteritems()
+        }
+
+
+    def load_config(self):
+        if os.path.isfile(self.config_path) is False:
+            shutil.copy(self.defaults_path, self.config_path)
+
+        self.config = utilities.load_toml_file(self.config_path)
+        if not self.config:
+            print("Could not load bringme defaults")
+
+    def save_config(self):
+        utilities.save_toml_file(self.config, self.config_path)
+
+
+# control.non_ccr_app_rule(BringRule(), context=None, rdp=False)
