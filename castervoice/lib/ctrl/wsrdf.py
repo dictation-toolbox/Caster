45b2c03d15c1d87d8b1f49520e8378dc362f35e1
Vojtěch Drábek <vojta.drabek@gmail.com>
2019-06-26 16:18:12 +0200

    Use Dragonfly's new timers and update Sikuli code (#577)
    * Use Dragonfly timers
    
    * Make Sikuli work with the new timer
    
    Made the whole thing a class in order to avoid globals
    
    * Remove no longer needed TimerForWSR
    
    * Update pip requirements
    
    * Do not display loading socket error again

--- castervoice/lib/ctrl/wsrdf.py
+++ castervoice/lib/ctrl/wsrdf.py
@@ -1,8 +1,3 @@
-import logging
-from threading import Timer
-import time
-
-
 class RecognitionHistoryForWSR(list):
     '''
     Copied verbatim from Dragonfly, but doesn't require Natlink
@@ -27,60 +22,3 @@ class RecognitionHistoryForWSR(list):
 
     def _recognition_to_item(self, words):
         return tuple(words)
-
-
-class TimerForWSR(object):
-    '''
-    Copied verbatim from Dragonfly, but doesn't require
-    Natlink and has been those parts reimplemented    
-    '''
-
-    class Callback(object):
-        def __init__(self, function, interval):
-            self.function = function
-            self.interval = interval
-            self.next_time = time.clock() + self.interval
-
-        def call(self):
-            self.next_time += self.interval
-            try:
-                self.function()
-            except Exception, e:
-                logging.getLogger("timer").exception("Exception during timer callback")
-                print("Exception during timer callback: %s (%r)" % (e, e))
-
-    def __init__(self, interval):
-        self.interval = interval
-        self.callbacks = []
-        self._continue = {"_continue": False}
-
-    def add_callback(self, function, interval):
-        self.callbacks.append(self.Callback(function, interval))
-        if len(self.callbacks) == 1:
-            self.setTimerCallback(self.callback)
-
-    def remove_callback(self, function):
-        for c in self.callbacks:
-            if c.function == function: self.callbacks.remove(c)
-        if len(self.callbacks) == 0:
-            self.setTimerCallback(None)
-
-    def callback(self):
-        now = time.clock()
-        for c in self.callbacks:
-            if c.next_time < now: c.call()
-
-    def setTimerCallback(self, callback):
-        _continue = self._continue
-        if callback is None:
-            _continue["_continue"] = False
-        else:
-            _continue["_continue"] = True
-            _interval = self.interval
-
-            def call():
-                if _continue["_continue"]:
-                    callback()
-                    Timer(_interval, call).start()
-
-            Timer(_interval, call).start()
