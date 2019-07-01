d0f72daa34f056476fbbeb5c3cce8cb511fef45c
Mike Roberts <42875462+mrob95@users.noreply.github.com>
2019-07-01 21:31:55 +0100

    Add line to enable Natlink

--- install.bat
+++ install.bat
@@ -5,8 +5,11 @@ echo Installation path: %currentpath%
 
 cd C:\Python27
 
-rem echo Installing caster dependencies
-rem python -m pip install -r "%currentpath%requirements.txt"
+echo Installing caster dependencies
+python -m pip install -r "%currentpath%requirements.txt"
+
+echo Setting natlink user directory
+python C:\NatLink\NatLink\confignatlinkvocolaunimacro\natlinkconfigfunctions.py -e
 
 echo Setting natlink user directory
 python C:\NatLink\NatLink\confignatlinkvocolaunimacro\natlinkconfigfunctions.py -n "%currentpath:~0,-1%"
