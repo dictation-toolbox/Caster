5cd51087577420794a2b5bb4ef7f507ff920b736
LexiconCode <thequietplace@protonmail.com>
2019-07-02 09:43:50 -0500

    Added rules and filters to user directory (#619)
    * Transfer filter rules from old master branch
    
    * Add  "FILTER_RULES_DEFAULTS_PATH" and RULES_DEFAULTS_PATH
    
    Renamed settings for "FILTER_DEFS_PATH" to "FILTER_WORDS_DEFS_PATH"
    modified gfilter to "FILTER_WORDS_DEFS_PATH"
    changed folder name to FilterRules
    
    * added function to copy default filters to user filters
    
    * fixed imports
    
    * Added examples copy user rule examples into user directory
    
     Clarified instructions for filter rules
    * Modified example rules and filters to use get_filter for now.

--- castervoice/lib/settings.py
+++ castervoice/lib/settings.py
@@ -191,13 +191,17 @@ _DEFAULT_SETTINGS = {
             _USER_DIR + "/data/bringme.toml",
         "BRINGME_DEFAULTS_PATH":
             BASE_PATH + "/bin/share/bringme.toml.defaults",
+        "FILTER_RULES_DEFAULTS_PATH":
+            BASE_PATH + "/bin/share/FilterRules",
+        "RULES_RULES_DEFAULTS_PATH":
+            BASE_PATH + "/bin/share/Rules",
         "ALIAS_PATH":
             _USER_DIR + "/data/aliases.toml",
         "CCR_CONFIG_PATH":
             _USER_DIR + "/data/ccr.toml",
         "DLL_PATH":
             BASE_PATH + "/lib/dll/",
-        "FILTER_DEFS_PATH":
+        "FILTER_WORDS_DEFS_PATH":
             _USER_DIR + "/data/words.txt",
         "LOG_PATH":
             _USER_DIR + "/log.txt",
