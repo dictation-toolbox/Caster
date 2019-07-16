3128a77dc9a57f51192ef7c3afbb3e6b015fddff
LexiconCode <thequietplace@protonmail.com>
2019-07-16 00:07:26 -0500

    Add store and retrieve

--- castervoice/apps/gitter.py
+++ castervoice/apps/gitter.py
@@ -5,20 +5,30 @@ Official Site "https://gitter.im/"
 """
 from castervoice.lib.imports import *
 
+
 class GitterRule(MergeRule):
     pronunciation = "Gitter"
 
     mapping = {
-        "bold": R(Text("****") + Key("left:2")),
-        "emphasize": R(Text("**") + Key("left")),
-        # "header":           R(Text( "" )), # H1 ## H2 ### H3
-        "insert item": R(Text("* ")),
-        "block quote": R(Text("> ")),
-        "mention": R(Text("@")),
-        "insert link": R(Text("[]()") + Key("left:3")),
-        "insert image": R(Text("![]()") + Key("left:3")),
-        "insert code": R(Text("``") + Key("left")),
-        "formatted code": R(Text("```") + Key("s-enter")),
+        "bold":
+            R(Store() + Text("**") + Retrieve() + Text("**")),
+        "emphasize":
+            R(Store() + Text("*") + Retrieve() + Text("*")),
+        # "header":  R(Text( "" )), # H1 ## H2 ### H3
+        "insert item":
+            R(Text("* ")),
+        "block quote":
+            R(Text("> ")),
+        "mention":
+            R(Store() + Text("@") + Retrieve()),
+        "insert link":
+            R(Store() + Text("[") + Retrieve() + Text("]()") + Key("left:2")),
+        "insert image":
+            R(Store() + Text("![") + Retrieve() + Text("]()") + Key("left:2")),
+        "insert code":
+            R(Store() + Text("`") + Retrieve() + Text("`")),
+        "formatted code":
+            R(Text("```") + Key("s-enter")),
     }
     extras = [
         Dictation("text"),
