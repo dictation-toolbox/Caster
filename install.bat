1ce7f168000a5f1a94024c9a44eb3664ffb4c71b
Mike Roberts <42875462+mrob95@users.noreply.github.com>
2019-07-26 21:11:59 +0100

    Update installation instructions

--- install.bat
+++ install.bat
@@ -20,11 +20,13 @@ cd C:\Python27
 echo Installing caster dependencies
 python -m pip install -r "%currentpath%requirements.txt"
 
+IF EXIST "C:\NatLink\" (
 echo Setting natlink user directory
 python C:\NatLink\NatLink\confignatlinkvocolaunimacro\natlinkconfigfunctions.py -e
 
 echo Setting natlink user directory
 python C:\NatLink\NatLink\confignatlinkvocolaunimacro\natlinkconfigfunctions.py -n "%currentpath:~0,-1%"
+)
 
 echo Caster installed at %currentpath%
 
