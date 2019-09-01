cb21a72336e89beeac303c1aa925051d672e1fa4
dave <dconway1985@gmail.com>
2019-09-01 14:02:25 -0400

    refs #385: fix concurrency problem

--- castervoice/lib/ctrl/mgr/loading/reload/base_reload_observable.py
+++ castervoice/lib/ctrl/mgr/loading/reload/base_reload_observable.py
@@ -1,5 +1,6 @@
 import hashlib
 import os
+from threading import Lock
 
 from castervoice.lib import printer
 
@@ -10,19 +11,30 @@ class BaseReloadObservable(object):
     that a file or directory needs reloading.
 
     Is called either by a timer or by a Dragonfly command.
+    In the timer case, the lock is needed to ensure that scanning for
+    updates doesn't happen while rules are being registered.
+    A less flexible alternative to this would be to not allow
+    the '_update' method to do anything until the GrammarManager
+    has registered all the rules. However, if rules are allowed to
+    be registered post-boot in the future, we'd be back to the
+    same problem of needing the lock.
     """
 
     def __init__(self):
         self._file_hashes = {}
         self._listeners = []
+        self._lock = Lock()
 
     def register_listener(self, listener):
         self._listeners.append(listener)
 
     def register_watched_file(self, file_path):
+        self._lock.acquire()
         self._file_hashes[file_path] = BaseReloadObservable._get_hash_of_file(file_path)
+        self._lock.release()
 
     def _update(self):
+        self._lock.acquire()
         for file_path in self._file_hashes:
             if not os.path.exists(file_path):
                 printer.out(
@@ -34,6 +46,7 @@ class BaseReloadObservable(object):
             if known_file_hash != current_file_hash:
                 self._file_hashes[file_path] = current_file_hash
                 self._notify_listeners(file_path)
+        self._lock.release()
 
     def _notify_listeners(self, path_changed):
         for listener in self._listeners:
