74ba5a368d9c952ea863ec84a28c57478c315918
synkarius <dconway1985@gmail.com>
2019-09-25 21:08:11 -0400

    refs #385, readability improvement to printer.out

--- castervoice/lib/printer.py
+++ castervoice/lib/printer.py
@@ -3,4 +3,4 @@ def out(*args):
     Use this as a printing interface to send messages to places other than the console.
     DO NOT import anything in this class. Use *args.
     """
-    print(args)
+    print("\n".join([str(o) for o in args]))
