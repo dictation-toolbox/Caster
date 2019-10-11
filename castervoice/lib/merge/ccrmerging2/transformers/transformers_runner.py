59559ae6ce6392b00e841183f12ee0861ecb6276
synkarius <dconway1985@gmail.com>
2019-10-10 23:01:36 -0400

    refs #385, #23: post transformer validations

--- castervoice/lib/merge/ccrmerging2/transformers/transformers_runner.py
+++ castervoice/lib/merge/ccrmerging2/transformers/transformers_runner.py
@@ -3,8 +3,10 @@ import traceback
 from dragonfly import Function, MappingRule
 
 from castervoice.lib import printer
+from castervoice.lib.ctrl.mgr.errors.invalid_transformation_error import InvalidTransformationError, ITMessage
 from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
 from castervoice.lib.merge.ccrmerging2.activation_rule_generator import ActivationRuleGenerator
+from castervoice.lib.merge.mergerule import MergeRule
 
 
 class TransformersRunner(ActivationRuleGenerator):
@@ -50,14 +52,39 @@ class TransformersRunner(ActivationRuleGenerator):
 
         return TransformersActivationRule, details
 
-    def transform_rule(self, rule):
-        r = rule
+    def transform_rule(self, rule_instance):
+        r = rule_instance
+        orig_class = TransformersRunner._get_rule_class(r)
         for transformer in self._transformers:
             try:
                 r = transformer.get_transformed_rule(r)
+                TransformersRunner._post_transform_validate(orig_class, r)
             except:
                 err = "Error while running transformer {} with {} rule."
                 printer.out(err.format(transformer, r))
-                import traceback
                 traceback.print_exc()
         return r
+
+    @staticmethod
+    def _get_rule_class(rule):
+        return rule.__class__
+
+    @staticmethod
+    def _post_transform_validate(orig_class, rule):
+        """
+        There are only two changes you're not allowed to make to a rule.
+        1. You're not allowed to change it into something which is not still its original class
+           or a child class of its original class.
+        2. You're not allowed to change the name of its class.
+        """
+        orig_rcn = orig_class.__name__
+        current_class = TransformersRunner._get_rule_class(rule)
+
+        no_class_change = orig_class == current_class
+        maintains_mapping_rule = issubclass(orig_class, MappingRule) and issubclass(current_class, MappingRule)
+        maintains_merge_rule = issubclass(orig_class, MergeRule) and issubclass(current_class, MergeRule)
+        maintains = no_class_change or maintains_mapping_rule or maintains_merge_rule
+        if not maintains:
+            raise InvalidTransformationError(ITMessage.BAD_TYPE, orig_rcn)
+        if orig_rcn != current_class.__name__:
+            raise InvalidTransformationError(ITMessage.CLASS_KEY, orig_rcn)
