2513160713a174d64d9aaab9443b8bd44beaefb5
LexiconCode <thequietplace@protonmail.com>
2019-12-09 16:45:26 -0600

    Upon error status window will come to the forefront.

--- _caster.py
+++ _caster.py
@@ -3,10 +3,12 @@
 main Caster module
 Created on Jun 29, 2014
 '''
-
+import datetime
 import logging
-logging.basicConfig()
 from dragonfly import get_engine
+from dragonfly.windows.window import Window
+
+logging.basicConfig(format = "%(asctime)s")  
 
 from castervoice.lib import settings  # requires toml
 settings.initialize()
@@ -14,6 +16,24 @@ settings.initialize()
 from castervoice.lib.ctrl.dependencies import DependencyMan  # requires nothing
 DependencyMan().initialize()
 _NEXUS = None
+
+class LoggingHandler(logging.Handler):
+    def emit(self, record):
+        msg = self.format(record)
+        print("\n")
+        # Brings status window to the forefront upon error
+        if (settings.SETTINGS["miscellaneous"]["status_window_forefront_on_error"]):
+             # The window title is unique to Natlink
+            if (get_engine()._name == 'natlink'):
+                windows = Window.get_matching_windows(None, "Messages from Python Macros V")
+            if windows:
+                windows[0].set_foreground()
+
+logger1 = logging.getLogger('action')
+logger1.addHandler(LoggingHandler())
+
+logger2 = logging.getLogger('engine')
+logger2.addHandler(LoggingHandler())
 
 # Natlink with DNS requires 32-bit Python and Windows OS
 # ToDo: create then move to castervoice.lib.ctrl.engines
