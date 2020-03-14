ad10caf6f07e4b594ec5da3b69fac64c1a19967a
LexiconCode <thequietplace@protonmail.com>
2020-03-14 15:12:40 -0500

    Update Run_Caster_Kaldi.bat
    Simplify kaldi arguments

--- Run_Caster_Kaldi.bat
+++ Run_Caster_Kaldi.bat
@@ -4,8 +4,6 @@ echo Runnig Kaldi from Dragonfly CLI
 set currentpath=%~dp0
 
 TITLE Caster: Status Window
-cmd /c python -m dragonfly load _*.py --engine kaldi  --no-recobs-messages --engine-options " \
-    model_dir=kaldi_model_zamia \
-    vad_padding_end_ms=300"
+python -m dragonfly load _*.py --engine kaldi  --no-recobs-messages
 
 pause 1
