6def0b1d5139b8e1098466bca5f33f374242dc61
LexiconCode <thequietplace@protonmail.com>
2020-03-19 17:19:06 -0500

    Removed `cmd /c`  from .bat files

--- Run_Caster_DNS.bat
+++ Run_Caster_DNS.bat
@@ -4,6 +4,6 @@ echo Runnig DNS/DPI from Dragonfly CLI with Natlink.
 set currentpath=%~dp0
 
 TITLE Caster: Status Window
-cmd /c python -m dragonfly load --engine natlink _*.py --no-recobs-messages
+python -m dragonfly load --engine natlink _*.py --no-recobs-messages
 
 pause 1
--- Run_Caster_WSR.bat
+++ Run_Caster_WSR.bat
@@ -4,6 +4,6 @@ echo Running WRS from Dragonfly CLI.
 set currentpath=%~dp0
 
 TITLE Caster: Status Window
-cmd /c python -m dragonfly load --engine sapi5inproc _*.py --no-recobs-messages
+python -m dragonfly load --engine sapi5inproc _*.py --no-recobs-messages
 
 pause 1
