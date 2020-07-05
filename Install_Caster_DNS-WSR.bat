5c5c5669d027b6d886348bfb7fabb0844c97edbd
David Zurow <daanzu@users.noreply.github.com>
2020-07-05 15:45:05 -0400

    Add active python/pip info in install scripts (#828)

--- Install_Caster_DNS-WSR.bat
+++ Install_Caster_DNS-WSR.bat
@@ -1,6 +1,8 @@
 @echo off
 set currentpath=%~dp0
 echo Installation path: %currentpath%
+echo Using this python/pip:
+python -m pip -V
 
 echo Installing Caster Dependencies for DNS/WSR
 python -m pip install -r "%currentpath%requirements.txt"
