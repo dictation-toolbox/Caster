d888731f529594a35abd1d5e1f67f1b0bae50c56
Kendon Bell <kmb56@berkeley.edu>
2020-08-21 15:32:12 +1200

    trying different encoding

--- castervoice/asynch/mouse/legion.py
+++ castervoice/asynch/mouse/legion.py
@@ -191,7 +191,7 @@ class LegionScanner:
         bbstring = self.tirg_dll.getTextBBoxesFromBytes(img.tobytes(), img.size[0],
                                                         img.size[1])
         # clean the results in case any garbage letters come through
-        result = re.sub("[^0-9,]", "", bbstring.decode("utf-8"))
+        result = re.sub("[^0-9,]", "", bbstring.decode(sys.getfilesystemencoding()))
         return result
 
     def scan(self, bbox=None, rough=True):
