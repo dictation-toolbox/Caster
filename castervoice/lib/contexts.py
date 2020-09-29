84bf67713329e4718a467ce2b7aed01f8a191f93
Kendon Bell <kendonB@users.noreply.github.com>
2020-09-29 22:28:42 +1300

    Add contexts for cross platform (#851)

--- castervoice/lib/contexts.py
+++ castervoice/lib/contexts.py
@@ -1,11 +1,20 @@
+import os
+import sys
+from dragonfly import FuncContext
 from castervoice.lib.context import AppContext
+
+WINDOWS_CONTEXT = FuncContext(lambda: sys.platform == "win32")
+MACOS_CONTEXT = FuncContext(lambda: sys.platform == "darwin")
+LINUX_CONTEXT = FuncContext(lambda: sys.platform.startswith("linux"))
+X11_CONTEXT = FuncContext(lambda: os.environ.get("XDG_SESSION_TYPE") == "x11")
 
 TERMINAL_CONTEXT = AppContext(executable=[
     "\\sh.exe",
     "\\bash.exe",
     "\\cmd.exe",
     "\\mintty.exe",
-    "\\powershell.exe"
+    "\\powershell.exe",
+    "gnome-terminal"
     ])
 
 JETBRAINS_CONTEXT = AppContext(executable="idea", title="IntelliJ") \
