813bd8c4118585fd0bc9ef3c2492d6f464b7d1a8
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2020-09-29 18:42:41 +0300

    snippet_utilities: add function for creating regular expressions

--- castervoice/rules/apps/editor/sublime_rules/snippet_utilities.py
+++ castervoice/rules/apps/editor/sublime_rules/snippet_utilities.py
@@ -1,6 +1,6 @@
 def placeholder(field,default  = ""):
 	"""Utility to generate snippet code for placeholders fields at runtime
-	
+
 	placeholder(1) = $1
 	placeholder(1,"data") = ${1:data}
 	placeholder((1,2),"data")  = ${1:${2:data}}
@@ -22,4 +22,30 @@ def placeholder(field,default  = ""):
 		else:
 			return "${" + str(field) + ":" + str(default)  + "}"
 
+def regular(varname,regex,format_string,option  = ""):
+	"""Utility for generating snippet code for regular expression substitution.Produces
+
+	${var_name/regex/format_string/options} or
+	${var_name/regex/format_string}
+
+	all arguments must be able to get casted into str()
+
+	Args:
+	    varname (str): The variable name for example 1,2
+	    regex (str): Perl style regular expression
+	    format_string (str): Perl style format string
+	    option (str, optional): optional can take one of the following values
+	    	- "i" : case insensitive
+	    	- "g" : replace all appearances
+	    	- "m" : do not ignore new lines
+
+	
+	Returns: str
+	"""
+	arg = (varname,regex,format_string) + ((option,) if option else ())
+	return "${" + "/".join(map(str, arg)) + "}"
+
+regex = {
+	
+}
 
