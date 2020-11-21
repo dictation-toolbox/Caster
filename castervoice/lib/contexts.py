cbd68e4c548ee3b0b9afb67588cd6f89d82cdb14
Kendon Bell <bellk@landcareresearch.co.nz>
2020-11-22 12:48:43 +1300

    add os functions

--- castervoice/lib/contexts.py
+++ castervoice/lib/contexts.py
@@ -3,10 +3,22 @@ import sys
 from dragonfly import FuncContext
 from castervoice.lib.context import AppContext
 
-WINDOWS_CONTEXT = FuncContext(lambda: sys.platform == "win32")
-MACOS_CONTEXT = FuncContext(lambda: sys.platform == "darwin")
-LINUX_CONTEXT = FuncContext(lambda: sys.platform.startswith("linux"))
-X11_CONTEXT = FuncContext(lambda: os.environ.get("XDG_SESSION_TYPE") == "x11")
+def is_windows():
+    return sys.platform == "win32"
+
+def is_macos():
+    return sys.platform == "darwin"
+
+def is_linux():
+    return sys.platform.startswith("linux")
+
+def is_x11():
+    return os.environ.get("XDG_SESSION_TYPE") == "x11"
+
+WINDOWS_CONTEXT = FuncContext(is_windows)
+MACOS_CONTEXT = FuncContext(is_macos)
+LINUX_CONTEXT = FuncContext(is_linux)
+X11_CONTEXT = FuncContext(is_x11)
 
 TERMINAL_CONTEXT = AppContext(executable=[
     "\\sh.exe",
