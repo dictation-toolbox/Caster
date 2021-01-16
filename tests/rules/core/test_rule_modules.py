e245dca9103f9cea137125b9a6b8d3ae22a9ae92
LexiconCode <CasterVoice@protonmail.com>
2021-01-16 11:40:44 -0600

    add keyboard to unit tests

--- tests/rules/core/test_rule_modules.py
+++ tests/rules/core/test_rule_modules.py
@@ -5,6 +5,7 @@ class CoreModulesTestCase(ModulesTestCase):
     def _rule_modules(self):
         from castervoice.rules.core.text_manipulation_rules import text_manipulation
         from castervoice.rules.core.numbers_rules import numeric
+        from castervoice.rules.core.keyboard_rules import keyboard
         from castervoice.rules.core.navigation_rules import nav
         from castervoice.rules.core.navigation_rules import nav2
         from castervoice.rules.core.navigation_rules import window_mgmt_rule
@@ -14,6 +15,6 @@ class CoreModulesTestCase(ModulesTestCase):
         from castervoice.rules.core.utility_rules import hardware_rule
         from castervoice.rules.core.utility_rules import mouse_alts_rules
 
-        return [alphabet, nav, nav2, window_mgmt_rule, numeric, punctuation,
+        return [alphabet, nav, nav2, window_mgmt_rule, numeric, keyboard, punctuation,
                 text_manipulation, caster_rule,
                 hardware_rule, mouse_alts_rules]
