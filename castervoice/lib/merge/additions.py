e804471c624d35810e2d3993e520bd8898f50d0d
LexiconCode <CasterVoice@protonmail.com>
2021-02-12 12:15:49 -0600

    Add warning message for deprecated IntegerRefST

--- castervoice/lib/merge/additions.py
+++ castervoice/lib/merge/additions.py
@@ -1,10 +1,18 @@
 from dragonfly import ShortIntegerRef
 from dragonfly.grammar.elements import Choice
+from castervoice.lib import settings 
 
-IntegerRefST = ShortIntegerRef
-# Compatibility shim for older grammars that use IntegerRefST.
-# IntegerRefST and Integer Remap has deprecated.
-# Use dragonfly ShortIntegerRef
+
+class IntegerRefST(ShortIntegerRef):
+    """
+    Compatibility shim for older grammars that use IntegerRefST.
+    IntegerRefST and Integer Remap has been removed use dragonfly ShortIntegerRef
+    """
+
+    def __init__(self, name, min, max, default=None):
+        message = "Detected 'IntegerRefST' import in rules/grammars.\nIntegerRefST and Integer Remap has been removed. \nUpdate your rules to `from dragonfly import ShortIntegerRef` in instead of IntegerRefST" 
+        settings.add_message(message)
+        super(IntegerRefST, self).__init__(name, min, max, default)
 
 
 class Boolean(Choice):
