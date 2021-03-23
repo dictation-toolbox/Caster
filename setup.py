46adf61c778d92e90257053fec674b128835090f
David Conway <2197849+synkarius@users.noreply.github.com>
2021-03-23 14:20:08 -0400

    Update setup.py
    Use CasterVoice@protonmail.com instead.

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setuptools.setup(
     name="castervoice",
     version=find_version("castervoice/lib", "version.py"),
     author="synkarius",
-    author_email="caster@donotrackplus.com",
+    author_email="CasterVoice@protonmail.com",
     description="Dragonfly-Based Voice Programming Toolkit",
     long_description=long_description,
     long_description_content_type="text/markdown",
