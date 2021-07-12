519d1da921c0ead0eb049c633a2ba1082170e641
David Conway <2197849+synkarius@users.noreply.github.com>
2021-07-12 19:00:01 -0400

    Issue 770 (#904)

--- castervoice/lib/merge/ccrmerging2/transformers/text_replacer/tr_item.py
+++
@@ -1,6 +0,0 @@
-class TRItem(object):
-    def __init__(self):
-        self.original = None  # original spec from Caster
-        self.extras = []  # bound extras' words
-        self.cleaned = None  # original with bound extras replaced with '?'s
-        self.altered = None  # 'cleaned' after replacement process
