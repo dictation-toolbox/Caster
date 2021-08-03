ea0ccf29aa6a96b39727aa04e6b9115e350932b1
synkarius <david@explosionduck.com>
2021-08-03 15:28:13 -0400

    fix relative import

--- castervoice/lib/settings.py
+++ castervoice/lib/settings.py
@@ -506,7 +506,7 @@ def initialize():
         sys.path.append(_debugger_path)
 
     # set up printer -- it doesn't matter where you do this; messages will start printing to the console after this
-    from printer import SimplePrintMessageHandler
+    from castervoice.lib.printer import SimplePrintMessageHandler
     dh = printer.get_delegating_handler()
     dh.register_handler(SimplePrintMessageHandler())
     dh.start()
