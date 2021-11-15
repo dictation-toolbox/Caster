53d1f5901de2398aec1de933b6e9081647452e58
LexiconCode <CasterVoice@protonmail.com>
2021-11-14 18:02:59 -0600

    Implement DictationSinkRule for Exclusivity Manager (#915)

--- castervoice/lib/ctrl/mgr/exclusivity_manager.py
+++ castervoice/lib/ctrl/mgr/exclusivity_manager.py
@@ -1,4 +1,4 @@
-from dragonfly import get_engine
+from dragonfly import get_engine, Mimic, MimicFailure
 from castervoice.lib import printer
 
 
@@ -8,9 +8,9 @@ class ExclusiveManager(object):
     Loads and switches exclusivity for caster modes
     """
     def __init__(self):
-        self.sleep_grammar = None
         self._get_engine = get_engine
-        self._set_sleep_grammar()
+        self.rule_names = ["CasterMicRule", "DictationSinkRule"]
+        self.exclusive_grammars = {}
 
     def set_mode(self, mode, modetype):
         """
@@ -19,26 +19,43 @@ class ExclusiveManager(object):
         :param modetype: 'mic_mode' or 'engine_mode' str
         """
         if modetype == "mic_mode":
+            self._get_grammars_loaded()
             if mode == "sleeping":
-                self._set_grammar_exclusivity(self.sleep_grammar, True)
+                self._set_grammars_exclusivity(self.exclusive_grammars, True)
                 printer.out("Caster: Microphone is sleeping")
             if mode == "on":
-                self._set_grammar_exclusivity(self.sleep_grammar, False)
+                self._set_grammars_exclusivity(self.exclusive_grammars, False)
                 printer.out("Caster: Microphone is on")
             if mode == "off":
                 printer.out("Caster: Microphone is off")
         else:
             printer.out("{}, {} not implemented".format(mode, modetype))
 
-    def _set_sleep_grammar(self):
-        for grammar in self._get_engine().grammars:
-            for rule in grammar.rules:
-                if rule.exported and "CasterMicRule" in str(rule):
-                    self.sleep_grammar = grammar
-                    return
-        else:
-            printer.out("Caster: ExclusiveManager 'CasterMicRule' grammar not found, is it active? say 'enable caster mic modes'")
+    def _get_grammars_loaded(self):
+        self.exclusive_grammars.clear()
+        for rule_name in self.rule_names:
+            for grammar in get_engine().grammars:
+                for rule in grammar.rules:
+                    if rule.exported and rule_name in str(rule):
+                        self.exclusive_grammars.update({rule_name: grammar})
+            if rule_name not in self.exclusive_grammars:
+                self._activate_grammar(rule_name)
 
-    def _set_grammar_exclusivity(self, grammar, state):
-        grammar.set_exclusive(state)
+    def _activate_grammar(self, rule_name):
+        # TODO: Find another way to activate grammars.
+        # Mimic cannot be used activate a grammar if DNS/DPI in sleep mode already.
+        # To reproduce disable `DictationSinkRule` and try to activate sleep mode.
+        try:
+            if str(rule_name) == "CasterMicRule":
+                Mimic("enable", "caster", "mic", "modes").execute()
+            if str(rule_name) == "DictationSinkRule":
+                Mimic("enable", "dictation", "sink", "rule").execute()
+            self._get_grammars_loaded()
+        except MimicFailure as e:
+            printer.out("Caster: {}".format(e))
+
+
+    def _set_grammars_exclusivity(self, grammars, state):
+        for rule_name, grammar in grammars.items():
+            grammar.set_exclusive(state)
         self._get_engine().process_grammars_context()
