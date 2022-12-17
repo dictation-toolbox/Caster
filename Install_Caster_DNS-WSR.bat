88ea6ffa5c09e3d6f5fa6920188f43faa0397f3a
LexiconCode <CasterVoice@protonmail.com>
2022-12-17 12:27:31 -0600

    Use py launcher in Install_Caster_DNS-WSR.bat

--- Install_Caster_DNS-WSR.bat
+++ Install_Caster_DNS-WSR.bat
@@ -2,9 +2,9 @@
 set currentpath=%~dp0
 echo Installation path: %currentpath%
 echo Using this python/pip:
-python -m pip -V
+py -m pip -V
 
 echo Installing Caster Dependencies for DNS/WSR
-python -m pip install -r "%currentpath%requirements.txt"
+py -m pip install -r "%currentpath%requirements.txt"
 
 pause 1
