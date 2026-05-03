d23432c06f1f5c2309fe8d3003401143a80622db
Amir Farhadi <amirf147@gmail.com>
2026-05-03 03:27:07 +0300

    Fix virtual desktop switching on Windows 11 by using updated pyvda API

--- castervoice/lib/windows_virtual_desktops.py
+++ castervoice/lib/windows_virtual_desktops.py
@@ -1,29 +1,31 @@
 from dragonfly import Window, Key
 from ctypes import windll
-# https://github.com/mrob95/py-VirtualDesktopAccessor
+
+# https://github.com/mirober/pyvda
 try:
-    import pyvda  # pylint: disable=import-error
+    from pyvda import VirtualDesktop, AppView, get_virtual_desktops
 except Exception as e:
     # This could fail on linux or windows <10
     print("Importing package pyvda failed with exception %s" % str(e))
 
 ASFW_ANY = -1
 
-
 def go_to_desktop_number(n):
     # Helps make sure that the target desktop gets focus
     windll.user32.AllowSetForegroundWindow(ASFW_ANY)
-    pyvda.GoToDesktopNumber(n)
-
+    VirtualDesktop(n).go()
 
 def move_current_window_to_desktop(n=1, follow=False):
-    window_handle = Window.get_foreground().handle
-    pyvda.MoveWindowToDesktopNumber(window_handle, n)
+    hwnd = Window.get_foreground().handle
+    AppView(hwnd).move(VirtualDesktop(n))
     if follow:
         go_to_desktop_number(n)
 
-
 def close_all_workspaces():
-    total = pyvda.GetDesktopCount()
-    go_to_desktop_number(total)
-    Key("wc-f4/10:" + str(total-1)).execute()
+    desktops = get_virtual_desktops()
+    total = len(desktops)
+    if total <= 1:
+        print("Only one desktop exists; nothing to close.")
+        return
+    go_to_desktop_number(total - 1)
+    Key("wc-f4/10:" + str(total - 1)).execute()
