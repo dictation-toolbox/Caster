7a442ed6111b3d01e805f25faaceb7cd7ddd8f08
Majira <JirA44@users.noreply.github.com>
2026-06-07 12:32:42 +0200

    fix: address review feedback for #814

---
+++ castervoice/lib/ctrl/mgr/loading/load/__init__.py
@@ -0,0 +1,8 @@
+# Create a list of numbers
+numbers = [1, 2, 3, 4, 5]
+# Add 5 to the end of the list
+numbers.append(5)
+# Remove the last element from the list
+numbers.pop()
+# Print the list
+print(numbers)
